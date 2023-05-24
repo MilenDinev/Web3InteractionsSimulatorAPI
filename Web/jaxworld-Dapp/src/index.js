3d541b05dfedec8ffb96f6870594671ef317ae08
DoradoFi <jaxminersworld@gmail.com>
2023-05-25 02:53:52 +0300

    Updating compoments structure

--- Web/jaxworld-Dapp/src/index.js
+++ Web/jaxworld-Dapp/src/index.js
@@ -1,49 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './Components/Header';
-import Body from './Components/Body';
-import Footer from './Components/Footer';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ThirdwebProvider } from '@thirdweb-dev/react';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <ThirdwebProvider activeChain="avalanche-fuji" autoConnect={true} >
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-//   </ThirdwebProvider>
-// );
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThirdwebProvider activeChain="avalanche-fuji" autoConnect={true} >
   <React.StrictMode>
-    <Header />
+    <App />
   </React.StrictMode>
   </ThirdwebProvider>
 );
 
 
-const hero = ReactDOM.createRoot(document.getElementById('hero'));
-hero.render(
-  <ThirdwebProvider activeChain="avalanche-fuji" autoConnect={true} >
-  <React.StrictMode>
-    <Body />
-  </React.StrictMode>
-  </ThirdwebProvider>
-);
-
-const footer = ReactDOM.createRoot(document.getElementById('footer'));
-footer.render(
-  <React.StrictMode>
-    <Footer />
-  </React.StrictMode>
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
