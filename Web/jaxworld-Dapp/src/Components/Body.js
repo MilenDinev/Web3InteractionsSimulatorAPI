e9edd2f78bce8606afbd6acd2012dcc2a8f47176
DoradoFi <jaxminersworld@gmail.com>
2023-05-25 22:52:43 +0300

    refactoring

--- Web/jaxworld-Dapp/src/Components/Body.js
+++ Web/jaxworld-Dapp/src/Components/Body.js
@@ -1,38 +1,28 @@
 import React from "react";
 import { Claim } from "./Helpers/renders/units/Claim";
 import { ContractData } from "./Helpers/renders/units/ContractData";
+import HeroContent from "./Helpers/renders/units/HeroContent";
 
 function Body() {
 
 
   return (
       <>
-      <Form/>
+      <BodyContent/>
       </>   
   );
 }
 
 
-function Form() {
+function BodyContent() {
   return (
     <div className="container col-xl-10 col-xxl-8 px-4 py-5">
-
-
-
-      <div id="hero" className="row align-items-center g-lg-5 py-5">
-
-        <div className="Fonts col-lg-7 text-center text-lg-start bg-gradient p-3 rounded-5 text-muted">
-          <h4 className="display-6 fw-bolder mb-2">Jax World's Genesis NFT Collection</h4>
-          <h3 className="display-6 fw-bold mb-1">G Minions</h3>
-          <h1 className="display-2 fw-bold lh-1 mb-0 text-white text-opacity-75">is now available</h1>
-          <p className="col-lg-10 fs-4">exclusively for our community members.</p>
-        </div> 
-
+      <div className="row align-items-center g-lg-5 py-5">
+          <HeroContent/>
         <div className="col-md-10 mx-auto col-lg-5">
           <form className="p-4 p-md-5 border border-secondary border-opacity-50 rounded-5 bg-transparent">
             <ContractData />
             <Claim />
-    
             <hr className="my-4"></hr>
             <div className="form-control border-0 bg-secondary bg-gradient rounded-4 p-2 text-muted bg-opacity-10 form-floating mb-3">
               <div className="form-check text-opacity-75">
@@ -42,7 +32,8 @@ function Form() {
                   value=""
                   id="flexCheckDefault"
                   required
-                ></input>
+                >                  
+                </input>
                 <label className="form-check-label" htmlFor="flexCheckDefault">
                   Agree with Privacy Policy
                 </label>
