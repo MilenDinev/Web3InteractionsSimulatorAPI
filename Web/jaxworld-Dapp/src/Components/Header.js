ee6ae2cd528b9464b647906ee4066d4c7bf82825
DoradoFi <jaxminersworld@gmail.com>
2023-05-27 02:46:23 +0300

    fixing header use of Fonts

--- Web/jaxworld-Dapp/src/Components/Header.js
+++ Web/jaxworld-Dapp/src/Components/Header.js
@@ -10,7 +10,7 @@ export function Header() {
     <header className="d-flex flex-wrap justify-content-center py-2 mb-3 border-bottom">
     <a href="index" target="_blank" rel="noreferrer" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
       <img className="bi me-2" width="45" height="45" src={logo} alt="Jax World"></img>
-      <span className="fs-2 Fonts hover-effect"><b>Jax World</b></span>
+      <span className="fs-2 fonts hover-effect"><b>Jax World</b></span>
     </a>
 
     <ul className="nav nav-pills">
