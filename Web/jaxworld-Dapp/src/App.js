dd0c9be0f1e0fc22545691d60924879fe4d4df0a
DoradoFi <jaxminersworld@gmail.com>
2023-06-03 23:29:18 +0300

    delete

--- Web/jaxworld-Dapp/src/App.js
+++ Web/jaxworld-Dapp/src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import Layout from './pages/Layout';
 
@@ -13,18 +13,18 @@ const NoPage = lazy(() => import('./pages/NoPage'));
 
 export default function App() {
     return (
-      <>
+      <BrowserRouter>
      <Suspense fallback={<div className="container">Loading...</div>}></Suspense>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="jax-world-dapp/mint" element={<Mint />} />
-            <Route path="jax-world-dapp/contact" element={<Contact />} />
-            <Route path="jax-world-dapp/terms-of-use" element={<Terms />} />
-            <Route path="jax-world-dapp/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="jax-world-dapp/*" element={<NoPage />} />
+            <Route path="/mint" element={<Mint />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/terms-of-use" element={<Terms />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/*" element={<NoPage />} />
           </Route>
         </Routes>
-        </>
+        </BrowserRouter>
     );
   }
