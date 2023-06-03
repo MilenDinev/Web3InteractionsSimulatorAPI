7ccea5b02f624b7bb5420cbed73c820731b5ae5c
DoradoFi <jaxminersworld@gmail.com>
2023-06-03 20:26:07 +0300

    refactoring

--- Web/jaxworld-Dapp/src/App.js
+++ Web/jaxworld-Dapp/src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
-import Layout from "./pages/Layout";
+import Layout from './pages/Layout';
 
 const Home = lazy(() => import('./pages/Home'));
 const Mint = lazy(() => import('./pages/Mint'));
