58ce9c248c9a22a4015781e286c7084eccd28644
DoradoFi <jaxminersworld@gmail.com>
2023-06-04 16:37:57 +0300

    update brand image and styles

--- Web/jaxworld-Dapp/src/Components/Footer.js
+++ Web/jaxworld-Dapp/src/Components/Footer.js
@@ -10,15 +10,15 @@ export function Footer() {
         <li className="ms-2" data-toggle="tooltip" title="Twitter"><Link className="text-muted" target="_blank" rel="noreferrer" to="https://twitter.com/jaxworld_"><i className="bi bi-twitter hover-effect" style={{ fontSize: 30 }}></i></Link></li>
         <li className="ms-2" data-toggle="tooltip" title="Medium"><Link className="text-muted" target="_blank" rel="noreferrer" to="https://medium.com/@jaxminersworld"><i className="bi bi-medium hover-effect" style={{ fontSize: 30 }}></i></Link></li>
       </ul>
-      <Link to="/" data-toggle="tooltip" title="Home" rel="noreferrer" className="col-md-5 d-flex justify-content-center text-decoration-none">
-        <p className="col-md-5 mb-0 text-muted">&copy; 2023 Jax World</p>
+      <Link to="/" data-toggle="tooltip" title="Home" rel="noreferrer" className="menu-button text-muted col-md-5 d-flex justify-content-center text-decoration-none">
+        <p className="col-md-5 mb-0 hover-effect">&copy; 2023 Jax World</p>
       </Link>
   
       <ul className="nav col-md-4 justify-content-end">
-        <li className="nav-item" data-toggle="tooltip" title="Contact Us"><Link to="contact" rel="noreferrer" className="menu-button nav-link px-2 text-muted hover-effect">Contact Us</Link></li>
-        <li className="nav-item" data-toggle="tooltip" title="FAQ"><Link to="faq" rel="noreferrer" className="menu-button nav-link px-2 text-muted hover-effect">FAQ</Link></li>
-        <li className="nav-item" data-toggle="tooltip" title="Terms of use"><Link to="terms-of-use" rel="noreferrer" className="menu-button nav-link px-2 text-muted hover-effect">Terms of Use</Link></li>
-        <li className="nav-item" data-toggle="tooltip" title="Privacy Policy"><Link to="privacy-policy" rel="noreferrer" className="menu-button nav-link px-2 text-muted hover-effect">Privacy policy</Link></li>
+        <li className="nav-item" data-toggle="tooltip" title="Contact Us"><Link to="contact" rel="noreferrer" className="menu-button nav-link px-2 text-muted"><label className="hover-effect">Contact Us</label></Link></li>
+        <li className="nav-item" data-toggle="tooltip" title="FAQ"><Link to="faq" rel="noreferrer" className="menu-button nav-link px-2 text-muted "><label className="hover-effect">FAQ</label></Link></li>
+        <li className="nav-item" data-toggle="tooltip" title="Terms of use"><Link to="terms-of-use" rel="noreferrer" className="menu-button nav-link px-2 text-muted"><label className="hover-effect">Terms of Use</label></Link></li>
+        <li className="nav-item" data-toggle="tooltip" title="Privacy Policy"><Link to="privacy-policy" rel="noreferrer" className="menu-button nav-link px-2 text-muted"><label className="hover-effect">Privacy policy</label></Link></li>
       </ul>
     </footer>
     </div>
