67cf1bffcec0072a858f20f14f52f49a0c15acfc
DoradoFi <jaxminersworld@gmail.com>
2023-06-07 22:05:24 +0300

    updating styles

--- Web/jaxworld-Dapp/src/Components/Footer.js
+++ Web/jaxworld-Dapp/src/Components/Footer.js
@@ -4,7 +4,7 @@ export function Footer() {
   return (
 
     <div className="container">
-    <footer className="d-flex flex-wrap justify-content-between align-items-center py-1 my-4 border-top  border-secondary footer-nav-main text">
+    <footer className=" d-flex flex-wrap justify-content-between align-items-center py-1 border-top  border-secondary footer-nav-main text">
       <ul className="nav col-md-3 justify-content-start">
         <li className="ms-2" data-toggle="tooltip" title="Discord"><Link className="text-muted" target="_blank" rel="noreferrer" to="https://discord.com/invite/dPNE6fK4S4"><i className="bi bi-discord hover-effect" style={{ fontSize: 30 }}></i></Link></li>
         <li className="ms-2" data-toggle="tooltip" title="Twitter"><Link className="text-muted" target="_blank" rel="noreferrer" to="https://twitter.com/jaxworld_"><i className="bi bi-twitter hover-effect" style={{ fontSize: 30 }}></i></Link></li>
@@ -14,7 +14,7 @@ export function Footer() {
         <p className="col-md-5 mb-0 hover-effect">&copy; 2023 Jax World</p>
       </Link>
   
-      <ul className="nav col-sm-4 col-md-3 col-lg-3 col-xl-4 col-xxl-4 justify-content-end">
+      <ul className=" nav col-sm-4 col-md-3 col-lg-3 col-xl-4 col-xxl-4 justify-content-end">
         <li className="nav-item" data-toggle="tooltip" title="Contact Us"><Link to="contact" rel="noreferrer" className="menu-button nav-link px-2 text-muted"><label className="hover-effect">Contact Us</label></Link></li>
         <li className="nav-item" data-toggle="tooltip" title="FAQ"><Link to="https://docs.jaxworld.xyz/faq" target="_blank" rel="noreferrer" className="menu-button nav-link px-2 text-muted "><label className="hover-effect">FAQ</label></Link></li>
         <li className="nav-item" data-toggle="tooltip" title="Terms of use"><Link to="terms-of-use" rel="noreferrer" className="menu-button nav-link px-2 text-muted"><label className="hover-effect">Terms of Use</label></Link></li>
--- Web/jaxworld-Dapp/src/pages/Home.js
+++ Web/jaxworld-Dapp/src/pages/Home.js
@@ -10,10 +10,10 @@ export default class Home extends Component {
   render(){
   return (
     <>
-    <div className="container welcome-screen">
-      <div className="row welcome-screen text">
-        <label>
-        <p>Page Under Construction</p>
+    <div className="container mbl-end-xl text-fonts text-l-space py-auto">
+      <div className="row mx-auto text-color text-center">
+        <label className="mx-auto bg-box-classic-an">
+        <h1 className="txt-f-s-xl-b text-center my-auto px-3 py-2">Page Under Construction</h1>
         </label>     
       </div>
     </div>
