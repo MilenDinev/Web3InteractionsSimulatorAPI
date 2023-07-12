5912ba7cc1968025ebd9a68f72e70ac30a284905
MilenDinev <dinev.milen@gmail.com>
2023-07-12 16:37:03 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ChainModels/CreateChainModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JaxWorld.Models.Requests.BlockchainRequests.ChainModels
+{
+    internal class CreateChainModel
+    {
+        public string Name { get; init; }
+        public string RpcUrl { get; init; }
+        public string ChainId { get; init; }
+        public string Symbol { get; init; }
+        public string ExplorerUrl { get; init; }
+    }
+}
