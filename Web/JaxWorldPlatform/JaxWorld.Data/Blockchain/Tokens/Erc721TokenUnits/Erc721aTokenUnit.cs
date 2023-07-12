30959dfe8a41b649b912bbf4463368e19bd69de3
MilenDinev <dinev.milen@gmail.com>
2023-07-12 18:00:10 +0300

    Class renaming

--- Web/JaxWorldPlatform/JaxWorld.Data/Blockchain/Tokens/Erc721TokenUnits/Erc721aTokenUnit.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Blockchain/Tokens/Erc721TokenUnits/Erc721aTokenUnit.cs
@@ -3,9 +3,9 @@
     using Properties;
     using Interfaces.IBlockchain.ITokens;
 
-    public class Erc721aT : Token, IErc721aT
+    public class Erc721aTokenUnit : Token, IErc721aT
     {
-        public Erc721aT()
+        public Erc721aTokenUnit()
         {
             Attributes = new HashSet<Attribute>();
             Utilities = new HashSet<Utility>();
