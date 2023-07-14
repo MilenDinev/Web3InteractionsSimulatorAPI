10f4c4ca7e88a3915cda4676d4252cbef02d5753
MilenDinev <dinev.milen@gmail.com>
2023-07-14 21:37:46 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Configurations/Provider/ProviderConfig.cs
@@ -0,0 +1,31 @@
+﻿namespace JaxWorld.Data.Entities.Blockchain.Configurations.Provider
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    internal class ProviderConfig : IEntityTypeConfiguration<Wallets.Provider>
+    {
+        public void Configure(EntityTypeBuilder<Wallets.Provider> builder)
+        {
+            builder.HasData(
+                    new Wallets.Provider
+                    {
+                        Id = 1,
+                        Name = "Metamask"
+                    });
+            builder.HasData(
+                    new Wallets.Provider
+                    {
+                        Id = 2,
+                        Name = "Coinbase"
+                    });
+
+            builder.HasData(
+                    new Wallets.Provider
+                    {
+                        Id = 3,
+                        Name = "WalletConnect"
+                    });
+        }
+    }
+}
