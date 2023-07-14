48d5589d6bc98dc3d7f180268457babfe5bbbdc2
MilenDinev <dinev.milen@gmail.com>
2023-07-14 21:41:04 +0300

    Change the way Provider class is envoke

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Configurations/Provider/ProviderConfig.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Configurations/Provider/ProviderConfig.cs
@@ -2,26 +2,27 @@
 {
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Wallets;
 
-    internal class ProviderConfig : IEntityTypeConfiguration<Wallets.Provider>
+    internal class ProviderConfig : IEntityTypeConfiguration<Provider>
     {
-        public void Configure(EntityTypeBuilder<Wallets.Provider> builder)
+        public void Configure(EntityTypeBuilder<Provider> builder)
         {
             builder.HasData(
-                    new Wallets.Provider
+                    new Provider
                     {
                         Id = 1,
                         Name = "Metamask"
                     });
             builder.HasData(
-                    new Wallets.Provider
+                    new Provider
                     {
                         Id = 2,
                         Name = "Coinbase"
                     });
 
             builder.HasData(
-                    new Wallets.Provider
+                    new Provider
                     {
                         Id = 3,
                         Name = "WalletConnect"
