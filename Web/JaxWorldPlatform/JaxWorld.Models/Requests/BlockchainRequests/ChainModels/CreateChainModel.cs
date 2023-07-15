ff0f8e3929e9973676fbbb536fd260adfd64a14a
MilenDinev <dinev.milen@gmail.com>
2023-07-16 01:36:15 +0300

    Rename ChainId property to NetworkId

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ChainModels/CreateChainModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ChainModels/CreateChainModel.cs
@@ -1,10 +1,10 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.ChainModels
 {
-    internal class CreateChainModel
+    public class CreateChainModel
     {
         public string Name { get; init; }
         public string RpcUrl { get; init; }
-        public string ChainId { get; init; }
+        public string NetworkId { get; init; }
         public string Symbol { get; init; }
         public string ExplorerUrl { get; init; }
     }
