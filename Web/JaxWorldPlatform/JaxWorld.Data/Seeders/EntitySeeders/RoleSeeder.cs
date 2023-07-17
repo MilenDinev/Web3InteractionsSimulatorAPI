04ebe982a10c6531c6b24c1bf56ee823d35f4d95
MilenDinev <dinev.milen@gmail.com>
2023-07-17 22:35:57 +0300

    Seed admin and regular user roles

--- Web/JaxWorldPlatform/JaxWorld.Data/Seeders/EntitySeeders/RoleSeeder.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Seeders/EntitySeeders/RoleSeeder.cs
@@ -1,6 +1,28 @@
 ﻿namespace JaxWorld.Data.Seeders.EntitySeeders
 {
-    internal class RoleSeeder
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+
+    public static class RolesSeeder
     {
+        public static async Task SeedRolesAsync(RoleManager<IdentityRole<int>> roleManager)
+        {
+            var adminRole = new IdentityRole<int>()
+            {
+                Name = "admin",
+                NormalizedName = "admin".ToUpper(),
+                ConcurrencyStamp = Guid.NewGuid().ToString("D")
+            };
+            await roleManager.CreateAsync(adminRole);
+
+            var regularRole = new IdentityRole<int>()
+            {
+                Name = "regular",
+                NormalizedName = "regular".ToUpper(),
+                ConcurrencyStamp = Guid.NewGuid().ToString("D")
+            };
+            await roleManager.CreateAsync(regularRole);
+        }
     }
 }
