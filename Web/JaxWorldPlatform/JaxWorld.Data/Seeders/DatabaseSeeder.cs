1e62d2fb2876a6573f0c814beb5f93ceade67749
MilenDinev <dinev.milen@gmail.com>
2023-07-18 22:32:18 +0300

    Initial creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Seeders/DatabaseSeeder.cs
@@ -0,0 +1,36 @@
+﻿namespace JaxWorld.Data.Seeders
+{
+    using System;
+    using System.Linq;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using Data.Entities;
+    using EntitySeeders;
+    using SampleSeeders;
+
+    [ExcludeFromCodeCoverage]
+    public static class DatabaseSeeder
+    {
+        public async static Task SeedAsync(IServiceProvider applicationServices)
+        {
+            using (IServiceScope serviceScope = applicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<JaxWorldDbContext>();
+                var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<int>>>();
+                var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<User>>();
+
+                await context.Database.MigrateAsync();
+
+                if (!context.Users.Any())
+                {
+                    await RolesSeeder.SeedRolesAsync(roleManager);
+                    await UsersSeeder.SeedUsersAsync(userManager);
+                    await ChainsSampleSeeder.Seed(context);
+                }
+            }
+        }
+    }
+}
---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Seeders/SampleSeeders/ChainsSampleSeeder.cs
@@ -0,0 +1,33 @@
+﻿namespace JaxWorld.Data.Seeders.SampleSeeders
+{
+    using Entities.Blockchain;
+    public static class ChainsSampleSeeder
+    {
+        public static async Task Seed(JaxWorldDbContext context)
+        {
+            var avalancheMainet = new Network
+
+            {
+                Name = "Avalanche Network",
+                RpcUrl= "https://api.avax.network/ext/bc/C/rpc",
+                ChainId= "43114",
+                Symbol= "AVAX",
+                ExplorerUrl= "https://snowtrace.io/ "
+            };
+
+            var avalancheFujiTestNet = new Network
+            {
+                Name = "Avalanche FUJI C-Chain",
+                RpcUrl = "https://api.avax-test.network/ext/bc/C/rpc",
+                ChainId = "43113",
+                Symbol = "AVAX",
+                ExplorerUrl = "https://testnet.snowtrace.io/"
+            };
+
+
+            await context.Networks.AddRangeAsync(avalancheMainet, avalancheFujiTestNet);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
