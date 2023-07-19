a9e177f68e3ca7569bfb0cc8d5fb5c34306bbbba
MilenDinev <dinev.milen@gmail.com>
2023-07-19 21:35:23 +0300

    Initial creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace JaxWorld.Data.Entities
+{
+    using System;
+    using Interfaces.IEntities;
+
+    public abstract class Entity : IEntity
+    {
+        public int Id { get; set; }
+        public string NormalizedName { get; set; }
+        public int CreatorId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int LastModifierId { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
