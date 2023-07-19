12ccea15a2fc8495ef318d9ac87c13298f9998a0
MilenDinev <dinev.milen@gmail.com>
2023-07-20 00:27:37 +0300

    Initial Creation

--- Web/JaxWorldPlatform/JaxWorld.Services/Main/ContractService.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/ContractService.cs
@@ -1,6 +1,36 @@
 ﻿namespace JaxWorld.Services.Main
 {
-    internal class ContractService
+    using AutoMapper;
+    using Base;
+    using Data;
+    using Data.Entities.Blockchain.Contracts;
+    using Models.Requests.BlockchainRequests.ContractModels;
+
+    internal class ContractService : BaseService<Contract>
     {
+        private readonly IMapper mapper;
+
+        public ContractService(JaxWorldDbContext dbContext, IMapper mapper) : base(dbContext)
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task<Contract> CreateAsync(CreateContractModel contractModel, int creatorId)
+        {
+            var contract = mapper.Map<Contract>(contractModel);
+            await CreateEntityAsync(contract, creatorId);
+            return contract;
+        }
+
+        public async Task EditAsync(Contract contract, EditContractModel contractModel, int modifierId)
+        {
+            contract.Name = contractModel.Name;
+
+            await SaveModificationAsync(contract, modifierId);
+        }
+        public async Task DeleteAsync(Contract contract, int modifierId)
+        {
+            await DeleteEntityAsync(contract, modifierId);
+        }
     }
 }
