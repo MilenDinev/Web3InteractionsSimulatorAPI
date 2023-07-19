81058b4c7981c26f9679984acfdb2beb3b8c0ebc
MilenDinev <dinev.milen@gmail.com>
2023-07-19 21:35:56 +0300

    Updating properties

--- Web/JaxWorldPlatform/JaxWorld.Data/Seeders/Interfaces/IEntities/IBlockchain/IWallets/IWallet.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Seeders/Interfaces/IEntities/IBlockchain/IWallets/IWallet.cs
@@ -1,7 +1,5 @@
 ﻿namespace JaxWorld.Data.Interfaces.IEntities.IBlockchain.IWallets
 {
-    using Blockchain.Transactions;
-    using JaxWorld.Data.Entities.Blockchain.Transactions;
 
     public interface IWallet
     {
@@ -9,7 +7,5 @@
         decimal Balance { get; set; }
         int OwnerId { get; set; }
         int ProviderId { get; set; }
-
-        ICollection<Transaction> Transactions { get; set; }
     }
 }
