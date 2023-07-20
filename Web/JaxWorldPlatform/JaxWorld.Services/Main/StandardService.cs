8edbdb26e60a974f18b7f55d3e684e3ebe833946
MilenDinev <dinev.milen@gmail.com>
2023-07-21 00:15:26 +0300

    CRUD methods implementation

--- Web/JaxWorldPlatform/JaxWorld.Services/Main/StandardService.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/StandardService.cs
@@ -1,6 +1,36 @@
 ﻿namespace JaxWorld.Services.Main
 {
-    internal class StandardService
+    using AutoMapper;
+    using Base;
+    using Data;
+    using Data.Entities.Blockchain.Tokens;
+    using Models.Requests.BlockchainRequests.StandardModels;
+
+    internal class StandardService : BaseService<Standard>
     {
+        private readonly IMapper mapper;
+
+        public StandardService(JaxWorldDbContext dbContext, IMapper mapper) : base(dbContext)
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task<Standard> CreateAsync(CreateStandardModel standardModel, int creatorId)
+        {
+            var standard = mapper.Map<Standard>(standardModel);
+            await CreateEntityAsync(standard, creatorId);
+            return standard;
+        }
+
+        public async Task EditAsync(Standard standard, EditStandardModel standardModel, int modifierId)
+        {
+            standard.Name = standardModel.Name;
+
+            await SaveModificationAsync(standard, modifierId);
+        }
+        public async Task DeleteAsync(Standard standard, int modifierId)
+        {
+            await DeleteEntityAsync(standard, modifierId);
+        }
     }
 }
