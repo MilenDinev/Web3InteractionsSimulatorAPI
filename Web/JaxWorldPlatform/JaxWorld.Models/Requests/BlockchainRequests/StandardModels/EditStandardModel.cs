926d34b2ce738e19214bc91d05e1b6ccd5367ccd
MilenDinev <dinev.milen@gmail.com>
2023-07-21 00:13:21 +0300

    Initial Create

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/StandardModels/EditStandardModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaxWorld.Models.Requests.BlockchainRequests.StandardModels
+{
+    public class EditStandardModel
+    {
+        public string Name { get; set; }
+    }
+}
