03e9c044e5d4c1de7d1d2841dc415d7c9d51a70a
MilenDinev <dinev.milen@gmail.com>
2023-07-21 00:13:15 +0300

    Initial Create

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/StandardModels/CreateStandardModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaxWorld.Models.Requests.BlockchainRequests.StandardModels
+{
+    public class CreateStandardModel
+    {
+        public int ContractId { get; set; }
+        public string Name { get; set; }
+    }
+}
