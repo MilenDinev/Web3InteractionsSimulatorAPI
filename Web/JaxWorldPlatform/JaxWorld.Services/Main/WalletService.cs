c3799b0ae1b7d8b977849881070484ce773ba5ad
MilenDinev <dinev.milen@gmail.com>
2023-07-23 23:50:26 +0300

    Initial implementation

--- Web/JaxWorldPlatform/JaxWorld.Services/Main/WalletService.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/WalletService.cs
@@ -1,6 +1,36 @@
 ﻿namespace JaxWorld.Services.Main
 {
-    internal class WalletService
+    using AutoMapper;
+    using Base;
+    using Data;
+    using Data.Entities.Blockchain.Wallets;
+    using Models.Requests.BlockchainRequests.WalletModels;
+
+    internal class WalletService : BaseService<Wallet>
     {
+        private readonly IMapper mapper;
+
+        public WalletService(JaxWorldDbContext dbContext, IMapper mapper) : base(dbContext)
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task<Wallet> CreateAsync(CreateWalletModel walletModel, int creatorId)
+        {
+            var wallet = mapper.Map<Wallet>(walletModel);
+            await CreateEntityAsync(wallet, creatorId);
+            return wallet;
+        }
+
+        public async Task EditAsync(Wallet wallet, EditWalletModel walletModel, int modifierId)
+        {
+            wallet.Name = walletModel.Name;
+
+            await SaveModificationAsync(wallet, modifierId);
+        }
+        public async Task DeleteAsync(Wallet wallet, int modifierId)
+        {
+            await DeleteEntityAsync(wallet, modifierId);
+        }
     }
 }
