299c44f78406d24b2e3436dca38207def7d298fb
MilenDinev <dinev.milen@gmail.com>
2023-07-23 23:50:32 +0300

    Initial implementation

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/AttributeService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using JaxWorld.Data;
+using JaxWorld.Models.Requests.BlockchainRequests.ContractModels;
+using JaxWorld.Services.Base;
+using Attribute = JaxWorld.Data.Entities.Blockchain.Properties.Attribute;
+
+namespace JaxWorld.Services.Main
+{
+    internal class AttributeService : BaseService<Attribute>
+    {
+        private readonly IMapper mapper;
+
+        public AttributeService(JaxWorldDbContext dbContext, IMapper mapper) : base(dbContext)
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task<Attribute> CreateAsync(CreateContractModel contractModel, int creatorId)
+        {
+            var contract = mapper.Map<Attribute>(contractModel);
+            await CreateEntityAsync(contract, creatorId);
+            return contract;
+        }
+
+        public async Task EditAsync(Attribute contract, EditContractModel contractModel, int modifierId)
+        {
+            contract.Name = contractModel.Name;
+
+            await SaveModificationAsync(contract, modifierId);
+        }
+        public async Task DeleteAsync(Attribute contract, int modifierId)
+        {
+            await DeleteEntityAsync(contract, modifierId);
+        }
+    }
+}
