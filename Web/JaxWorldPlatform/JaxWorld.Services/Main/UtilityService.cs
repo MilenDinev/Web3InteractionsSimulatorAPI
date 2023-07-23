960f0e2b42e288f4406c281f88d17e880e06f549
MilenDinev <dinev.milen@gmail.com>
2023-07-23 23:52:31 +0300

    Initial implementation

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/UtilityService.cs
@@ -0,0 +1,36 @@
+﻿namespace JaxWorld.Services.Main
+{
+    using AutoMapper;
+    using Base;
+    using Data;
+    using Data.Entities.Blockchain.Properties;
+    using Models.Requests.BlockchainRequests.PropertiesModels;
+
+    internal class UtilityService : BaseService<Utility>
+    {
+        private readonly IMapper mapper;
+
+        public UtilityService(JaxWorldDbContext dbContext, IMapper mapper) : base(dbContext)
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task<Utility> CreateAsync(CreateUtilityModel utilityModel, int creatorId)
+        {
+            var utility = mapper.Map<Utility>(utilityModel);
+            await CreateEntityAsync(utility, creatorId);
+            return utility;
+        }
+
+        public async Task EditAsync(Utility utility, EditUtilityModel utilityModel, int modifierId)
+        {
+            utility.Type = utilityModel.Name;
+
+            await SaveModificationAsync(utility, modifierId);
+        }
+        public async Task DeleteAsync(Utility utility, int modifierId)
+        {
+            await DeleteEntityAsync(utility, modifierId);
+        }
+    }
+}
