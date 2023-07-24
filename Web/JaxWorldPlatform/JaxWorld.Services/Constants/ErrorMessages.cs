be8cda55687043295f732d43781445a5e7931653
MilenDinev <dinev.milen@gmail.com>
2023-07-25 00:00:13 +0300

    Removing unused messaged

--- Web/JaxWorldPlatform/JaxWorld.Services/Constants/ErrorMessages.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Constants/ErrorMessages.cs
@@ -1,8 +1,8 @@
 ﻿namespace JaxWorld.Services.Constants
 {
-    internal static class ErrorMessages
+    public static class ErrorMessages
     {
-        internal const string InvalidCredentials = "Invalid credentials, please try again!";
+        public const string InvalidCredentials = "Invalid credentials, please try again!";
         internal const string InvalidPublicationDate = "Please provide valid publication date in format 'dd/MM/yyyy'!";
         internal const string EntityDoesNotExist = @"{0} does not exist!";
         internal const string EntityIdDoesNotExist = @"{0} with id '{1}' does not exist!";
@@ -10,10 +10,5 @@
         internal const string EntityAlreadyAssignedId = @"{0} with id '{1}' has already been assigned to this {2}!";
         internal const string EntityAlreadyContained = @"{0} already exists in this collection!";
         internal const string EntityHasBeenDeleted = @"{0} has already been deleted and cannot be modified!";
-        internal const string AlreadyFavoriteId = @"{0} with id '{1}' already exists in favorites!";
-        internal const string NotFavoriteId = @"{0} with id '{1}' does not exists in favorites!";
-        internal const string AlreadyFollowing = @"{0} with id '{1}' has already been followed by you!";
-        internal const string NotFollowing = @"{0} with id '{1}' is not followed by you!";
-        internal const string FollowingItSelf = @"Following yourself is forbidden!";
     }
 }
