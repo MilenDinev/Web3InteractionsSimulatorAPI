7dd31ebf3a866a0703d6d0dd335d8a7aa396f732
MilenDinev <dinev.milen@gmail.com>
2023-07-24 13:56:40 +0300

    Initial implementation of mapping profile

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Network/NetworkMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿namespace JaxWorld.Services.AutoMapperProfiles.Network
+{
+    using AutoMapper;
+    using Data.Entities.Blockchain;
+    using Models.Requests.BlockchainRequests.ContractModels;
+    using Models.Responses.BlockchainResponses.ContractModels;
+
+    internal class NetworkMappingProfile : Profile
+    {
+        public NetworkMappingProfile()
+        {
+            this.CreateMap<CreateContractModel, Network>()
+                .ForMember(e => e.NormalizedName, m => m.MapFrom(m => m.Name.ToUpper()));
+            this.CreateMap<Network, CreatedContractModel>();
+            this.CreateMap<Network, EditedContractModel>();
+        }
+    }
+}
