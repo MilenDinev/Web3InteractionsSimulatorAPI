52737d62ad8dc3eac5a83f12e6b594e6791e8c95
MilenDinev <dinev.milen@gmail.com>
2023-07-24 13:07:55 +0300

    Initial Implementation

--- Web/JaxWorldPlatform/JaxWorld.Services/Main/TransactionService.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/TransactionService.cs
@@ -1,6 +1,39 @@
 ﻿namespace JaxWorld.Services.Main
 {
-    internal class TransactionService
+    using AutoMapper;
+    using Base;
+    using Data;
+    using Data.Entities.Blockchain.Transactions;
+    using Models.Requests.BlockchainRequests.TransactionModels;
+
+    internal class TransactionService : BaseService<Transaction>
     {
+        private readonly IMapper mapper;
+
+        public TransactionService(JaxWorldDbContext dbContext, IMapper mapper) : base(dbContext)
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task<Transaction> CreateAsync(CreateTransactionModel transactionModel, int creatorId)
+        {
+            var transaction = mapper.Map<Transaction>(transactionModel);
+            await CreateEntityAsync(transaction, creatorId);
+            return transaction;
+        }
+
+        public async Task EditAsync(Transaction transaction, EditTransactionModel transactionModel, int modifierId)
+        {
+            transaction.State = new TransactionState
+            {
+                Value = transactionModel.State
+        };
+
+            await SaveModificationAsync(transaction, modifierId);
+        }
+        public async Task DeleteAsync(Transaction transaction, int modifierId)
+        {
+            await DeleteEntityAsync(transaction, modifierId);
+        }
     }
 }
