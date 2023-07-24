ac3722295a8ab7a4adb0f185c53605934b4615db
MilenDinev <dinev.milen@gmail.com>
2023-07-25 00:00:37 +0300

    Initial creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/IEntityChecker.cs
@@ -0,0 +1,15 @@
+﻿using JaxWorld.Data.Interfaces.IEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaxWorld.Services.Handlers.Interfaces
+{
+    public interface IEntityChecker
+    {
+        Task<bool> NullableCheck<T>(T entity, string searchFlag) where T : class, IEntity;
+        Task<bool> DeletedCheck<T>(T entity) where T : class, IEntity;
+    }
+}
