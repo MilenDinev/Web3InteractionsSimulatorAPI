d7e2f7f493a954f4de27ef0cb637b17c21adc46e
MilenDinev <dinev.milen@gmail.com>
2023-07-24 23:54:17 +0300

    Initial Commit

---
+++ Web/JaxWorldPlatform/JaxWorld.Web/Controllers/NetworksController.cs
@@ -0,0 +1,22 @@
+﻿namespace JaxWorld.Web.Controllers
+{
+    using AutoMapper;
+    using Services.Main.Interfaces;
+    using Services.Handlers.Interfaces;
+    using Microsoft.AspNetCore.Mvc;
+    using Base;
+    using Data.Entities.Blockchain;
+    using Models.Requests.BlockchainRequests.ChainModels;
+    using Models.Responses.BlockchainResponses.ChainModels;
+    using Microsoft.AspNetCore.HttpOverrides;
+
+    // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NetworksController : JaxWorldBaseController
+    {
+
+    }
+}
+
