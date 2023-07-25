0f1f24a4a25c05aed65488f010e9033686645b57
MilenDinev <dinev.milen@gmail.com>
2023-07-26 02:16:35 +0300

    Removing Type property

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Interfaces/IEntities/IBlockchain/IProperties/IAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace JaxWorld.Data.Interfaces.IEntities.IBlockchain.IProperties
+{
+    public interface IAttribute
+    {
+        int ContractId { get; set; }
+        string Value { get; set; }
+    }
+}
---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Interfaces/IEntities/IBlockchain/IProperties/IUtility.cs
@@ -0,0 +1,8 @@
+﻿namespace JaxWorld.Data.Interfaces.IEntities.IBlockchain.IProperties
+{
+    public interface IUtility
+    {
+        int ContractId { get; set; }
+        decimal Value { get; set; }
+    }
+}
