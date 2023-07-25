2695b4c39969445b62217ab5c2e2cf046ff11880
MilenDinev <dinev.milen@gmail.com>
2023-07-26 01:35:20 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/ProfileModels/ProfileListingModel.cs
@@ -0,0 +1,12 @@
+﻿namespace JaxWorld.Models.Responses.BlockchainResponses.ProfileModels
+{
+    public class ProfileListingModel
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string Description { get; set; }
+        public int TotalSupply { get; set; }
+        public int StandardId { get; set; }
+        public int ContractId { get; set; }
+    }
+}
