9ff19a6e19ec630d055ea52715f8fa6184f4d9d6
MilenDinev <dinev.milen@gmail.com>
2023-07-25 21:55:17 +0300

    Introducing CreateAsync, EditAsync, DeleteAsync methods

--- Web/JaxWorldPlatform/JaxWorld.Services/Main/Interfaces/IContractService.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/Interfaces/IContractService.cs
@@ -1,6 +1,12 @@
 ﻿namespace JaxWorld.Services.Main.Interfaces
 {
+    using Data.Entities.Blockchain.Contracts;
+    using Models.Requests.BlockchainRequests.ContractModels;
+
     public interface IContractService
     {
+        Task<Contract> CreateAsync(CreateContractModel model, int creatorId);
+        Task EditAsync(Contract contract, EditContractModel model, int modifierId);
+        Task DeleteAsync(Contract contract, int modifierId);
     }
 }
