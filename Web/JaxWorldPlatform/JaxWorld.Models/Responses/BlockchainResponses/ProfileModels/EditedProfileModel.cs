7b83195d78ef3bce20b5d919cce194017ae0cf1f
MilenDinev <dinev.milen@gmail.com>
2023-07-26 00:51:55 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/ProfileModels/EditedProfileModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JaxWorld.Models.Responses.BlockchainResponses.ProfileModels
+{
+    public class EditedProfileModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string Description { get; set; }
+        public int TotalSupply { get; set; }
+    }
+}
