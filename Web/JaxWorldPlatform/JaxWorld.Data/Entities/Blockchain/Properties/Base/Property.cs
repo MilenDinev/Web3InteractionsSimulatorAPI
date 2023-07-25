c9b730c8b6dc43ab61d2b9367c0f0ad6f3231da2
MilenDinev <dinev.milen@gmail.com>
2023-07-26 02:15:52 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Properties/Base/Property.cs
@@ -0,0 +1,8 @@
+﻿namespace JaxWorld.Data.Entities.Blockchain.Properties.Base
+{
+    using Interfaces.IEntities.IBlockchain.IProperties;
+    public abstract class Property : Entity, IProperty
+    {
+        public string Type { get; set; }
+    }
+}
