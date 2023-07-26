f4aaec62e7240627b08ebb2bbcf8f8d9695d2fa1
MilenDinev <dinev.milen@gmail.com>
2023-07-27 02:36:52 +0300

    Add Create, Edit and Delete Methods

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/Interfaces/IUnitService.cs
@@ -0,0 +1,12 @@
+﻿namespace JaxWorld.Services.Main.Interfaces
+{
+    using Data.Entities.Blockchain.Base;
+    using Models.Requests.BlockchainRequests.UnitModels;
+
+    public interface IUnitService
+    {
+        Task<Unit> CreateAsync(CreateUnitModel model, int creatorId);
+        Task EditAsync(Unit unit, EditUnitModel model, int modifierId);
+        Task DeleteAsync(Unit unit, int modifierId);
+    }
+}
