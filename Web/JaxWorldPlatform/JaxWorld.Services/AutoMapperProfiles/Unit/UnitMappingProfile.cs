a05ad4d85fa258d928fe449aaea31b045c5a9b77
MilenDinev <dinev.milen@gmail.com>
2023-07-27 02:35:16 +0300

    Initializing initial mappings

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Unit/UnitMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿namespace JaxWorld.Services.AutoMapperUnits.Unit
+{
+    using AutoMapper;
+    using Unit = Data.Entities.Blockchain.Base.Unit;
+    using Models.Requests.BlockchainRequests.UnitModels;
+    using Models.Responses.BlockchainResponses.ProfileUnitModels;
+    using Models.Responses.BlockchainResponses.UnitModels;
+
+    internal class UnitMappingUnit : Profile
+    {
+        internal UnitMappingUnit()
+        {
+            this.CreateMap<CreateUnitModel, Unit>()
+                .ForMember(e => e.NormalizedName, m => m.MapFrom(m => m.Name.ToUpper()));
+            this.CreateMap<Unit, CreatedUnitModel>();
+            this.CreateMap<Unit, EditedUnitModel>();
+            this.CreateMap<Unit, DeletedUnitModel>();
+            this.CreateMap<Unit, UnitListingModel>();
+        }
+    }
+}
