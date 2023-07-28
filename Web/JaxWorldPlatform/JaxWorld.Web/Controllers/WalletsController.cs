c6edfac1cc1062df55942137cdac194ed1fcfd9e
MilenDinev <dinev.milen@gmail.com>
2023-07-28 23:54:31 +0300

    Refactoring

--- Web/JaxWorldPlatform/JaxWorld.Web/Controllers/WalletsController.cs
+++ Web/JaxWorldPlatform/JaxWorld.Web/Controllers/WalletsController.cs
@@ -9,36 +9,6 @@
     [ApiController]
     public class WalletsController : ControllerBase
     {
-        // GET: api/<WalletsController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/<WalletsController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<WalletsController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<WalletsController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<WalletsController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
