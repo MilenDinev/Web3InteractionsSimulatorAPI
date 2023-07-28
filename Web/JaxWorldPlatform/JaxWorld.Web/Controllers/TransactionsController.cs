4b8e277dd40b88e24c3a6e8beb3e70f6d4d6363d
MilenDinev <dinev.milen@gmail.com>
2023-07-28 23:54:52 +0300

    Refactoring

--- Web/JaxWorldPlatform/JaxWorld.Web/Controllers/TransactionsController.cs
+++ Web/JaxWorldPlatform/JaxWorld.Web/Controllers/TransactionsController.cs
@@ -8,36 +8,6 @@
     [ApiController]
     public class TransactionsController : ControllerBase
     {
-        // GET: api/<TransactionsController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/<TransactionsController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<TransactionsController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<TransactionsController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<TransactionsController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
