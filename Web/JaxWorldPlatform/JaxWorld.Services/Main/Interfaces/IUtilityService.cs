b6ab7130662c35b4e7a769ce7116ff8bcf1b3bb6
MilenDinev <dinev.milen@gmail.com>
2023-07-30 01:12:50 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/Interfaces/IUtilityService.cs
@@ -0,0 +1,10 @@
+﻿namespace JaxWorld.Services.Main.Interfaces
+{
+    using Base;
+    using Data.Entities.Blockchain.Properties;
+    using Models.Requests.BlockchainRequests.PropertiesModels;
+
+    public interface IUtilityService : IPropertyService<Utility, CreateUtilityModel, EditUtilityModel>
+    {
+    }
+}
