5f2ac96e29060947a5508a094b953370dba61d81
MilenDinev <dinev.milen@gmail.com>
2023-07-29 23:12:01 +0300

    Initial Commit

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/Base/CreatePropertyModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JaxWorld.Models.Requests.BlockchainRequests.PropertiesModels.Base
+{
+    using Interfaces;
+
+    public abstract class CreatePropertyModel<T> : ICreatePropertyModel<T>
+    {
+        public string Name { get; set; }
+        public T Value { get; set; }
+    }
+}
