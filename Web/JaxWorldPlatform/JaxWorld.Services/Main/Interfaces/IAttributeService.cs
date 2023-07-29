731ae7013c380f7988e2b1301b96dd0c2a56106c
MilenDinev <dinev.milen@gmail.com>
2023-07-29 18:32:23 +0300

    Initial Creation and inheriting from IProperty

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/Interfaces/IAttributeService.cs
@@ -0,0 +1,11 @@
+﻿namespace JaxWorld.Services.Main.Interfaces
+{
+    using Base;
+    using Data.Entities.Blockchain.Properties;
+    using Models.Requests.BlockchainRequests.PropertiesModels;
+
+    internal interface IAttributeService : IPropertyService<Attribute, CreateAttributeModel, EditAttributeModel>
+    {
+
+    }
+}
