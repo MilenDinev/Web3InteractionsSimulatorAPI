572a5bb028f69d536432d2466514acbdf3c448c9
MilenDinev <dinev.milen@gmail.com>
2023-07-29 18:32:50 +0300

    Moving in different directory

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/Interfaces/Base/IPropertyService.cs
@@ -0,0 +1,15 @@
+﻿namespace JaxWorld.Services.Main.Interfaces.Base
+{
+    using Models.Requests.BlockchainRequests.Interfaces;
+    using Data.Interfaces.IEntities.IBlockchain.IProperties;
+
+    internal interface IPropertyService<TProperty, TCModel, TEModel> 
+        where TProperty : class, IProperty 
+        where TCModel : class, ICreateModel
+        where TEModel : class, IEditModel
+    {
+        Task<TProperty> CreateAsync(TCModel model, int creatorId);
+        Task EditAsync(TProperty property, TEModel model, int modifierId);
+        Task DeleteAsync(TProperty property, int modifierId);
+    }
+}
