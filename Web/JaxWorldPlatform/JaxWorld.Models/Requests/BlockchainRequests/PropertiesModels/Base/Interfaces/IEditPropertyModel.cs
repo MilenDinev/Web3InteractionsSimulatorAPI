587f76147e2388c6f80025316d27c95925349871
MilenDinev <dinev.milen@gmail.com>
2023-07-29 23:11:53 +0300

    Initial Commit

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/Base/Interfaces/IEditPropertyModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JaxWorld.Models.Requests.BlockchainRequests.PropertiesModels.Base.Interfaces
+{
+    using BlockchainRequests.Interfaces;
+
+    public interface IEditPropertyModel<T> : IEditModel
+    {
+        public string Name { get; set; }
+        public T Value { get; set; }
+    }
+}
