d898bbd298530f8ea7de186d7f2911e0caf019ea
MilenDinev <dinev.milen@gmail.com>
2023-07-30 14:05:07 +0300

    Add properties

--- Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/TransactionModels/CreatedTransactionModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/TransactionModels/CreatedTransactionModel.cs
@@ -2,5 +2,9 @@
 {
     public class CreatedTransactionModel
     {
+        public int Id { get; set; }
+        public int ContractId { get; set; }
+        public string TxnHash { get; set; }
+        public string State { get; set; }
     }
 }
