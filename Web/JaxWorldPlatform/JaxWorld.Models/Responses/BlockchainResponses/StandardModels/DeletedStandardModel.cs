c5009640cf348bfff61d5d14b0eb63e987a820d1
MilenDinev <dinev.milen@gmail.com>
2023-07-30 13:55:53 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/StandardModels/DeletedStandardModel.cs
@@ -0,0 +1,8 @@
+﻿namespace JaxWorld.Models.Responses.BlockchainResponses.StandardModels
+{
+    public class DeletedStandardModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
