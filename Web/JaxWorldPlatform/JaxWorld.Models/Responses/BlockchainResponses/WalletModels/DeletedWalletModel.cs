971913a8e7492308f3fe2f42498037457e9a50d6
MilenDinev <dinev.milen@gmail.com>
2023-07-30 14:38:16 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/WalletModels/DeletedWalletModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JaxWorld.Models.Responses.BlockchainResponses.WalletModels
+{
+    public class DeletedWalletModel
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Provider { get; set; }
+        public string Owner { get; set; }
+    }
+}
---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/WalletModels/WalletListingModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JaxWorld.Models.Responses.BlockchainResponses.WalletModels
+{
+    public class WalletListingModel
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Provider { get; set; }
+        public decimal Balance { get; set; }
+        public string Owner { get; set; }
+    }
+}
