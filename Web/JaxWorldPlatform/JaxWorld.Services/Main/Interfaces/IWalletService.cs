fd1f3c22b672cd65271ceaa9a7bd8cf472ab49fd
MilenDinev <dinev.milen@gmail.com>
2023-07-30 14:43:18 +0300

    Updating access modifier and declaration of create, edit and delete method

--- Web/JaxWorldPlatform/JaxWorld.Services/Main/Interfaces/IWalletService.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/Interfaces/IWalletService.cs
@@ -1,6 +1,12 @@
 ﻿namespace JaxWorld.Services.Main.Interfaces
 {
-    internal interface IWalletService
+    using Data.Entities.Blockchain.Wallets;
+    using Models.Requests.BlockchainRequests.WalletModels;
+
+    public interface IWalletService
     {
+        Task<Wallet> CreateAsync(CreateWalletModel model, int creatorId);
+        Task EditAsync(Wallet wallet, EditWalletModel model, int modifierId);
+        Task DeleteAsync(Wallet wallet, int modifierId);
     }
 }
