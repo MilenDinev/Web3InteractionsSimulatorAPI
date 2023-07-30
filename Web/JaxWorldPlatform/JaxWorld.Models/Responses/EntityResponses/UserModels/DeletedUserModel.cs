16aeb37f1d5b2da219156a5a960c70d4f16aa0dc
MilenDinev <dinev.milen@gmail.com>
2023-07-30 14:57:34 +0300

    Initial creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/EntityResponses/UserModels/DeletedUserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JaxWorld.Models.Responses.EntityResponses.UserModels
+{
+    public class DeletedUserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string WalletAddress { get; set; }
+    }
+}
