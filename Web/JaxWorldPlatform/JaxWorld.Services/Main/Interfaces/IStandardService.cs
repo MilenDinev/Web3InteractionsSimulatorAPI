2ca344585f610700c45f8252464bd023ec3158e3
MilenDinev <dinev.milen@gmail.com>
2023-07-30 13:53:54 +0300

    Initial create and indtroducing Create, Edit and Delete methods

--- Web/JaxWorldPlatform/JaxWorld.Services/Main/Interfaces/IStandardService.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Main/Interfaces/IStandardService.cs
@@ -1,6 +1,12 @@
 ﻿namespace JaxWorld.Services.Main.Interfaces
 {
-    internal interface IStandardService
+    using Data.Entities.Blockchain;
+    using Models.Requests.BlockchainRequests.StandardModels;
+
+    public interface IStandardService
     {
+        Task<Standard> CreateAsync(CreateStandardModel model, int creatorId);
+        Task EditAsync(Standard standard, EditStandardModel model, int modifierId);
+        Task DeleteAsync(Standard standard, int modifierId);
     }
 }
