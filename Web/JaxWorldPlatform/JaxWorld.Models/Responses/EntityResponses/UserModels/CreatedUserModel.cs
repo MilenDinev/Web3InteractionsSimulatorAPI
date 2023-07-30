4357949986022482d2b51217a696b3e26ee396ba
MilenDinev <dinev.milen@gmail.com>
2023-07-30 14:56:36 +0300

    Adding Properties

--- Web/JaxWorldPlatform/JaxWorld.Models/Responses/EntityResponses/UserModels/CreatedUserModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/EntityResponses/UserModels/CreatedUserModel.cs
@@ -2,5 +2,10 @@
 {
     public class CreatedUserModel
     {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string WalletAddress { get; set; }
     }
 }
--- Web/JaxWorldPlatform/JaxWorld.Models/Responses/EntityResponses/UserModels/UserListingModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/EntityResponses/UserModels/UserListingModel.cs
@@ -2,5 +2,10 @@
 {
     public class UserListingModel
     {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string WalletAddress { get; set; }
     }
 }
