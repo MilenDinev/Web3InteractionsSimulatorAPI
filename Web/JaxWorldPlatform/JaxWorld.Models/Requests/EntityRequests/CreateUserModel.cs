a092a33aed6ddb64a9c5f775b87c559db91da33f
MilenDinev <dinev.milen@gmail.com>
2023-07-30 14:56:19 +0300

    Update properties

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/EntityRequests/CreateUserModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/EntityRequests/CreateUserModel.cs
@@ -7,12 +7,14 @@
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
-        [Required(ErrorMessage = "Email is required!")]
-        [EmailAddress(ErrorMessage = "Please provide valid email address!")]
-        public string Email { get; set; }
+        public string WalletAddress { get; set; }
 
         [Required(ErrorMessage = "Password is required!")]
         public string Password { get; set; }
     }
 }
+
+
+//[Required(ErrorMessage = "Email is required!")]
+//[EmailAddress(ErrorMessage = "Please provide valid email address!")]
+//public string Email { get; set; }
