253644a25cd5f9399f76115684ffab6aa6a306b2
MilenDinev <dinev.milen@gmail.com>
2023-08-01 23:39:15 +0300

    adding profile mappings

--- Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Wallet/WalletMappingProfile.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Wallet/WalletMappingProfile.cs
@@ -1,6 +1,20 @@
 ﻿namespace JaxWorld.Services.AutoMapperProfiles.Wallet
 {
-    internal class WalletMappingProfile
+    using AutoMapper;
+    using Data.Entities.Blockchain.Wallets;
+    using Models.Requests.BlockchainRequests.WalletModels;
+    using Models.Responses.BlockchainResponses.WalletModels;
+
+    internal class WalletMappingProfile : Profile
     {
+        internal WalletMappingProfile()
+        {
+            this.CreateMap<CreateWalletModel, Wallet>()
+                .ForMember(e => e.NormalizedName, m => m.MapFrom(m => m.Address.ToUpper()));
+            this.CreateMap<Wallet, CreatedWalletModel>();
+            this.CreateMap<Wallet, EditedWalletModel>();
+            this.CreateMap<Wallet, DeletedWalletModel>();
+            this.CreateMap<Wallet, WalletListingModel>();
+        }
     }
 }
