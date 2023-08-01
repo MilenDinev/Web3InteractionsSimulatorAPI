c1d5d2e5a15f521a0472b706f5322f5c430d4c84
MilenDinev <dinev.milen@gmail.com>
2023-08-01 23:34:07 +0300

    refactoring

--- Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/User/UserMappingProfile.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/User/UserMappingProfile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JaxWorld.Services.AutoMapperProfiles.User
+﻿namespace JaxWorld.Services.AutoMapperProfiles.User
 {
     internal class UserMappingProfile
     {
