b6307c72540ac7099eabdade15e9c532177ed9d5
MilenDinev <dinev.milen@gmail.com>
2023-08-01 23:39:34 +0300

    adding profile mappings

--- Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Transaction/TransactionMappingProfile.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Transaction/TransactionMappingProfile.cs
@@ -1,6 +1,20 @@
 ﻿namespace JaxWorld.Services.AutoMapperProfiles.Transaction
 {
-    internal class TransactionMappingProfile
+    using AutoMapper;
+    using Data.Entities.Blockchain.Transactions;
+    using Models.Requests.BlockchainRequests.TransactionModels;
+    using Models.Responses.BlockchainResponses.TransactionModels;
+
+    internal class TransactionMappingProfile : Profile
     {
+        internal TransactionMappingProfile()
+        {
+            this.CreateMap<CreateTransactionModel, Transaction>()
+                .ForMember(e => e.NormalizedName, m => m.MapFrom(m => m.State.ToUpper()));
+            this.CreateMap<Transaction, CreatedTransactionModel>();
+            this.CreateMap<Transaction, EditedTransactionModel>();
+            this.CreateMap<Transaction, DeletedTransactionModel>();
+            this.CreateMap<Transaction, TransactionListingModel>();
+        }
     }
 }
