a5c16207cf96ad5be201086650b3c9eb5483aee3
MilenDinev <dinev.milen@gmail.com>
2023-08-01 23:39:30 +0300

    adding profile mappings

--- Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/User/UserMappingProfile.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/User/UserMappingProfile.cs
@@ -1,6 +1,20 @@
 ﻿namespace JaxWorld.Services.AutoMapperProfiles.User
 {
-    internal class UserMappingProfile
+    using AutoMapper;
+    using Data.Entities;
+    using Models.Requests.EntityRequests;
+    using Models.Responses.EntityResponses.UserModels;
+
+    internal class UserMappingProfile : Profile
     {
+        internal UserMappingProfile()
+        {
+            this.CreateMap<CreateUserModel, User>()
+                .ForMember(e => e.NormalizedName, m => m.MapFrom(m => m.UserName.ToUpper()));
+            this.CreateMap<User, CreatedUserModel>();
+            this.CreateMap<User, EditedUserModel>();
+            this.CreateMap<User, DeletedUserModel>();
+            this.CreateMap<User, UserListingModel>();
+        }
     }
 }
