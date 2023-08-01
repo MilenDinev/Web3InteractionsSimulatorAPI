d76baa031cff658608d60b8197101f5df81ec827
MilenDinev <dinev.milen@gmail.com>
2023-08-01 23:34:14 +0300

    refactoring

--- Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Wallet/WalletMappingProfile.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Wallet/WalletMappingProfile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JaxWorld.Services.AutoMapperProfiles.Wallet
+﻿namespace JaxWorld.Services.AutoMapperProfiles.Wallet
 {
     internal class WalletMappingProfile
     {
