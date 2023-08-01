12db2f02c611a2a1fa636e7205919f82162f609a
MilenDinev <dinev.milen@gmail.com>
2023-08-01 23:25:07 +0300

    Create Attribute mapping profile

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Attribute/AttributeMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿namespace JaxWorld.Services.AutoMapperProfiles.Attribute
+{
+    using AutoMapper;
+    using Data.Entities.Blockchain.Properties;
+    using Models.Requests.BlockchainRequests.PropertiesModels;
+    using Models.Responses.BlockchainResponses.PropertiesModels.Attribute;
+
+    internal class AttributeMappingProfile : Profile
+    {
+        internal AttributeMappingProfile()
+        {
+            this.CreateMap<CreateAttributeModel, Attribute>()
+                .ForMember(e => e.NormalizedName, m => m.MapFrom(m => m.Name.ToUpper()));
+            this.CreateMap<Attribute, CreatedAttributeModel>();
+            this.CreateMap<Attribute, EditedAttributeModel>();
+            this.CreateMap<Attribute, DeletedAttributeModel>();
+            this.CreateMap<Attribute, AttributeListingModel>();
+        }
+    }
+}
