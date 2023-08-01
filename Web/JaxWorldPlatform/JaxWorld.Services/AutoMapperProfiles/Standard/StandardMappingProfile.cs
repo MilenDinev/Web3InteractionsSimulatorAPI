cac7f361beec336999718b81fa64e9547d88d464
MilenDinev <dinev.milen@gmail.com>
2023-08-01 23:33:36 +0300

    Implementing mapping profiles

--- Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Standard/StandardMappingProfile.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Standard/StandardMappingProfile.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JaxWorld.Services.AutoMapperProfiles.Standard
+﻿namespace JaxWorld.Services.AutoMapperProfiles.Standard
 {
-    internal class StandardMappingProfile
+    using AutoMapper;
+    using Data.Entities.Blockchain;
+    using Models.Responses.BlockchainResponses.StandardModels;
+    using Models.Requests.BlockchainRequests.StandardModels;
+
+    internal class StandardMappingProfile : Profile
     {
+        internal StandardMappingProfile()
+        {
+            this.CreateMap<CreateStandardModel, Standard>()
+                .ForMember(e => e.NormalizedName, m => m.MapFrom(m => m.Name.ToUpper()));
+            this.CreateMap<Standard, CreatedStandardModel>();
+            this.CreateMap<Standard, EditedStandardModel>();
+            this.CreateMap<Standard, DeletedStandardModel>();
+            this.CreateMap<Standard, StandardListingModel>();
+        }
     }
 }
