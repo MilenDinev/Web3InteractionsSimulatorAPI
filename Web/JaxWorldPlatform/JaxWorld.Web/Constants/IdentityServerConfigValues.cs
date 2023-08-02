82a0b11be5de30daae131d35d75e307cb5f3f34d
MilenDinev <dinev.milen@gmail.com>
2023-08-02 23:35:37 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Web/Constants/IdentityServerConfigValues.cs
@@ -0,0 +1,13 @@
+﻿namespace JaxWorld.Web.Constants
+{
+    public static class IdentityServerConfigValues
+    {
+        public const string UrlAddress = @"https://localhost:5001";
+        public const string ResourcesUrlAddress = @"https://localhost:5001/resources";
+        public const string TokenBaseAddress = @"https://localhost:5001/connect/token";
+        public const string ClientId = "jaxworld";
+        public const string SecretValue = "secret";
+        public const string GrantType = "password";
+        public const bool AllowOfflineAccess = true;
+    }
+}
