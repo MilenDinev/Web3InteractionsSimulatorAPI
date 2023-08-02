188f1745581de5a07925072d5fe6b737cf2ed1d0
MilenDinev <dinev.milen@gmail.com>
2023-08-02 23:36:31 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Web/Extensions/Authentication/AuthenticationConfigurator.cs
@@ -0,0 +1,23 @@
+﻿namespace JaxWorld.Web.Extensions.Authentication
+{
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Constants;
+
+    public static class AuthenticationConfigurator
+    {
+        public static void AddAuthenticationConfig(this IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = IdentityServerConfigValues.UrlAddress;
+                    options.Audience = IdentityServerConfigValues.ResourcesUrlAddress;
+                });
+        }
+    }
+}
