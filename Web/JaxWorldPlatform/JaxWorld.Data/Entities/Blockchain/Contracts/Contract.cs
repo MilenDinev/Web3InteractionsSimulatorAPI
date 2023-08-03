49124e8e78ef7d925fcbb69ded78ca651458fa3e
MilenDinev <dinev.milen@gmail.com>
2023-08-03 22:36:41 +0300

    Refactoring properties

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Contracts/Contract.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Contracts/Contract.cs
@@ -21,9 +21,7 @@
         public int ProfileId { get; set; }
         public virtual Profile Profile { get; set; }
         public int CreatedTxnId { get; set; }
-        public virtual Transaction CreatedTxn { get; set; }
         public virtual Wallet Creator { get; set; }
-        public int NetworkId { get; set; }
         public virtual ICollection<Network> Networks { get; set; }
         public virtual ICollection<Transaction> Transactions { get; set; }
     }
