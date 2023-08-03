be6a2bad6aa85740aa785e6c839c8c4beb557056
MilenDinev <dinev.milen@gmail.com>
2023-08-03 22:38:02 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/TokenModel.cs
@@ -0,0 +1,19 @@
+﻿namespace JaxWorld.Models.Responses
+{
+    using System.Text.Json.Serialization;
+
+    public class TokenModel
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonPropertyName("refresh_token")]
+        public string RefreshToken { get; set; }
+    }
+}
