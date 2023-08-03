25f273d1d9aa1da3459a0fad076b6f71fbb3217e
MilenDinev <dinev.milen@gmail.com>
2023-08-03 22:37:49 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Configurations/Contract/ContractTransactionConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace JaxWorld.Data.Entities.Blockchain.Configurations.Contract
+{
+    using Blockchain.Transactions;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class ContractTransactionConfig : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.HasOne(s => s.Contract)
+            .WithMany(u => u.Transactions)
+            .HasForeignKey(s => s.ContractId)
+            .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
