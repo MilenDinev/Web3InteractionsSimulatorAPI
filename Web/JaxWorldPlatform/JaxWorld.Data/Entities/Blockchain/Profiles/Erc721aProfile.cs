bd3660428599472cb2852bdc3684debf34445082
MilenDinev <dinev.milen@gmail.com>
2023-08-03 22:37:14 +0300

    Refactoring properties

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Profiles/Erc721aProfile.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Profiles/Erc721aProfile.cs
@@ -3,17 +3,20 @@
     using Base;
     using Units;
     using Interfaces.IEntities.IBlockchain.IProfiles;
+    using Blockchain.Wallets;
 
     public class Erc721aProfile : Profile, IErc721aProfile
     {
         public Erc721aProfile()
         {
             Units = new HashSet<Erc721aUnit>();
+            Holders = new HashSet<Wallet>();
         }
 
         public string Description { get; set; }
         public int TotalSupply { get; set; }
         public int TotalMinted { get; set; }
         public virtual ICollection<Erc721aUnit> Units { get; set; }
+        public virtual ICollection<Wallet> Holders { get; set; }
     }
 }
