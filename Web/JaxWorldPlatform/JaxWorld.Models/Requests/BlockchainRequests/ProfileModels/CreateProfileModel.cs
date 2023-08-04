326aced08c8cf27c85553eaab2893bd3e00d2e98
MilenDinev <dinev.milen@gmail.com>
2023-08-04 21:38:09 +0300

    Adding Required attribute

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/CreateProfileModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/CreateProfileModel.cs
@@ -1,9 +1,14 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.ProfileModels
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CreateProfileModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Symbol { get; set; }
+        [Required]
         public string Description { get; set; }
         public int TotalSupply { get; set; }
         public int StandardId { get; set; }
