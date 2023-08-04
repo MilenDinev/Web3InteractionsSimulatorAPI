9488f2eb25a867dba51a3f7720107f53aef261b1
MilenDinev <dinev.milen@gmail.com>
2023-08-04 21:38:25 +0300

    making TotalSupply property nullable

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/EditProfileModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/EditProfileModel.cs
@@ -5,6 +5,6 @@
         public string Name { get; set; }
         public string Symbol { get; set; }
         public string Description { get; set; }
-        public int TotalSupply { get; set; }
+        public int? TotalSupply { get; set; }
     }
 }
