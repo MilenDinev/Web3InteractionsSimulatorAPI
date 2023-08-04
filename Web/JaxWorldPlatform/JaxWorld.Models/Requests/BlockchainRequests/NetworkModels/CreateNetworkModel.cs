ee3594a861f2f39026034af20df20baf5e3fa817
MilenDinev <dinev.milen@gmail.com>
2023-08-04 22:18:52 +0300

    Adding Required Attribute

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/NetworkModels/CreateNetworkModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/NetworkModels/CreateNetworkModel.cs
@@ -1,11 +1,16 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.NetworkModels
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CreateNetworkModel
     {
+        [Required]
         public string Name { get; init; }
-        public string RpcUrl { get; init; }
-        public string NetworkId { get; init; }
+        [Required]
         public string Symbol { get; init; }
+        [Required]
+        public string RpcUrl { get; init; }
+        public int ChainId { get; init; }
         public string ExplorerUrl { get; init; }
     }
 }
