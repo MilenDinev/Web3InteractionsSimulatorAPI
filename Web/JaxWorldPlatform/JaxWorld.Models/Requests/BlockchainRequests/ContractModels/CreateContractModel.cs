1b0cc92c39ee74100cc5c4b0c6e195f2cad8df0f
MilenDinev <dinev.milen@gmail.com>
2023-08-04 21:37:51 +0300

    Adding Required attribute and changing a property value type

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/CreateContractModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/CreateContractModel.cs
@@ -1,9 +1,13 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.ContractModels
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CreateContractModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Address { get; set; }
-        public string Chain { get; set; }
+        public int ChainId { get; set; }
     }
 }
