67097ca6db5d93139c6b5080cc03c8594520ce05
MilenDinev <dinev.milen@gmail.com>
2023-08-05 17:06:09 +0300

    Adding  attributes for Initial validation

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/Base/CreatePropertyModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/Base/CreatePropertyModel.cs
@@ -1,9 +1,13 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.PropertiesModels.Base
 {
     using Interfaces;
+    using System.ComponentModel.DataAnnotations;
 
     public abstract class CreatePropertyModel : ICreatePropertyModel
     {
+        [Required(ErrorMessage = "Property name is required and must be between 2 and 15 symbols!")]
+        [MaxLength(15, ErrorMessage = "Property name is required and must be between 2 and 15 symbols!")]
+        [MinLength(2, ErrorMessage = "Property name is required and must be between 2 and 15 symbols!")]
         public string Name { get; set; }
     }
 }
