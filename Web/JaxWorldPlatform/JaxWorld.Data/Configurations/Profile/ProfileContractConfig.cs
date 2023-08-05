3ba148e78c61d6340644e012f498d7e28822c3b4
MilenDinev <dinev.milen@gmail.com>
2023-08-05 22:20:47 +0300

    Initial Create

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Profile/ProfileContractConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace JaxWorld.Data.Configurations.Profile
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Entities.Blockchain.Profiles;
+    using Entities.Blockchain.Contracts;
+
+    public class ProfileContractConfig : IEntityTypeConfiguration<Contract>
+    {
+        public void Configure(EntityTypeBuilder<Contract> builder)
+        {
+            builder.HasOne(s => s.Profile)
+            .WithOne(u => u.Contract)
+            .HasForeignKey<Profile>(e => e.ContractId)
+            .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
