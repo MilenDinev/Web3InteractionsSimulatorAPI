72c42bc994d32ec0541e370def0236dd112e0d28
MilenDinev <dinev.milen@gmail.com>
2023-08-05 17:07:17 +0300

    Adding  attributes for Initial validation

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/UnitModels/CreateErc721aUnitModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/UnitModels/CreateErc721aUnitModel.cs
@@ -4,7 +4,9 @@ namespace JaxWorld.Models.Requests.BlockchainRequests.UnitModels
 {
     public class CreateErc721aUnitModel
     {
-        [Required]
+        [Required(ErrorMessage = "Unit name is required and must be between 2 and 15 symbols!")]
+        [MaxLength(15, ErrorMessage = "Unit name is required and must be between 2 and 15 symbols!")]
+        [MinLength(2, ErrorMessage = "Unit name is required and must be between 2 and 15 symbols!")]
         public string Name { get; set; }
         public int ProfileId { get; set; }
         public int MintedTxnId { get; set; }
