74107c2aec82ced729d0303dda25b5ac89b6f520
MilenDinev <dinev.milen@gmail.com>
2023-08-05 23:33:06 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Web/Controllers/AthTokenController.cs
@@ -0,0 +1,32 @@
+﻿namespace JaxWorld.Web.Controllers
+{
+    using Base;
+    using Extensions.Authentication;
+    using Services.Main.Interfaces;
+    using Models.Responses;
+    using Models.Requests.EntityRequests;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AthTokenController : JaxWorldBaseController
+    {
+        public AthTokenController(IUserService userService) : base(userService)
+        {
+        }
+
+        /// <summary>
+        /// Generate token given valid credentials
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost("GenerateToken/")]
+        public async Task<ActionResult<TokenModel>> GenerateToken(UserTokenRequestModel user)
+        {
+            using var httpClientHandler = new HttpClientHandler();
+            using var client = new HttpClient(httpClientHandler);
+            var token = await this.userService.GetEligibilityTokenAsync(client, user.UsernameOrEmail, user.Password);
+            return token;
+        }
+    }
+}
