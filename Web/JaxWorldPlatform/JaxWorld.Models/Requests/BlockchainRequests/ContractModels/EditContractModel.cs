b23e8f1e7b59b3f15968924c111fd46fe9dccba6
MilenDinev <dinev.milen@gmail.com>
2023-08-05 17:04:58 +0300

    Adding  attributes for Initial validation

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/EditContractModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/EditContractModel.cs
@@ -4,7 +4,9 @@
 
     public class EditContractModel
     {
-        [Required]
+        [Required(ErrorMessage = "Contract name is required and must be between 2 and 15 symbols!")]
+        [MaxLength(15, ErrorMessage = "Contract name is required and must be between 2 and 15 symbols!")]
+        [MinLength(2, ErrorMessage = "Contract name is required and must be between 2 and 15 symbols!")]
         public string Name { get; set; }
     }
 }
