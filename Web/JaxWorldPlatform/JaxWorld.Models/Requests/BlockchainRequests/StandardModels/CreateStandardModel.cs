1f423199b23655e9bc8d05e96163dbd79cbebc48
MilenDinev <dinev.milen@gmail.com>
2023-08-05 17:06:30 +0300

    Adding  attributes for Initial validation

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/StandardModels/CreateStandardModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/StandardModels/CreateStandardModel.cs
@@ -4,7 +4,9 @@ namespace JaxWorld.Models.Requests.BlockchainRequests.StandardModels
 {
     public class CreateStandardModel
     {
-        [Required]
+        [Required(ErrorMessage = "Standard name is required and must be between 2 and 15 symbols!")]
+        [MaxLength(15, ErrorMessage = "Standard name is required and must be between 2 and 15 symbols!")]
+        [MinLength(2, ErrorMessage = "Standard name is required and must be between 2 and 15 symbols!")]
         public string Name { get; set; }
     }
 }
