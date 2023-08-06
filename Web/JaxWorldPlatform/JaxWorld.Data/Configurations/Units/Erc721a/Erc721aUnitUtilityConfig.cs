91c601dd1d82988a24b6ed3abd3bfa900098289a
MilenDinev <dinev.milen@gmail.com>
2023-08-06 21:42:40 +0300

    Initial creation and configuration of database relation between Erc721aUnit and Utility entities

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Units/Erc721a/Erc721aUnitUtilityConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace JaxWorld.Data.Configurations.Units.Erc721a
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Entities.Blockchain.Units;
+    using Entities.Blockchain.Properties;
+
+    public class Erc721aUnitUtilityConfig : IEntityTypeConfiguration<Utility>
+    {
+        public void Configure(EntityTypeBuilder<Utility> builder)
+        {
+            builder.HasMany(g => g.Erc721AUnits)
+             .WithMany(u => u.Utilities)
+                .UsingEntity<Dictionary<string, object>>("Erc721aUnitsUtilities",
+                x => x.HasOne<Erc721aUnit>().WithMany().HasForeignKey("Erc721aUnitId")
+                      .OnDelete(DeleteBehavior.Restrict),
+                x => x.HasOne<Utility>().WithMany().HasForeignKey("UtilityId")
+                      .OnDelete(DeleteBehavior.Restrict));
+        }
+    }
+}
