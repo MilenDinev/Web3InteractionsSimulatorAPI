ca5d7e6e58a507e593ed714bb614a8484a8f3a87
MilenDinev <dinev.milen@gmail.com>
2023-08-06 21:41:21 +0300

    Initial creation and configuration of database relation between Standard and Profile entities

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Standard/StandardProfileConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace JaxWorld.Data.Configurations.Standard
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Entities.Blockchain.Profiles;
+
+    public class StandardProfileConfig : IEntityTypeConfiguration<Profile>
+    {
+        public void Configure(EntityTypeBuilder<Profile> builder)
+        {
+            builder.HasOne(s => s.Standard)
+            .WithMany(u => u.Profiles)
+            .HasForeignKey(s => s.StandardId)
+            .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
