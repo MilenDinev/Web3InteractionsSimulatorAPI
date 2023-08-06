b8285be7d3389e1f3f286256a4ff43caeaa9e7ae
MilenDinev <dinev.milen@gmail.com>
2023-08-06 21:53:44 +0300

    making ContractId nullable and adding Erc721aUnit property

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Transactions/Transaction.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Transactions/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿namespace JaxWorld.Data.Entities.Blockchain.Transactions
 {
+    using Units;
     using Contracts;
     using Blockchain.Wallets;
     using Interfaces.IEntities.IBlockchain.ITransactions;
@@ -10,9 +11,11 @@
         public int StateId { get; set; }
         public virtual TransactionState State { get; set; }
         public virtual Wallet Creator { get; set; }
-        public int ContractId { get; set; }
+        public int? ContractId { get; set; }
         public virtual Contract Contract { get; set; }
         public int NetworkId { get; set; }
         public virtual Network Network { get; set; }
+        public int? Erc721aUnitId { get; set; }
+        public virtual Erc721aUnit Erc721aUnit { get; set; }
     }
 }
