052846737e42bebb535ba6c5cc792e4bccc18a4a
MilenDinev <dinev.milen@gmail.com>
2023-08-06 22:01:49 +0300

    Adding explicit mapping for Provider Balance and Owner properties

--- Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Wallet/WalletMappingProfile.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Wallet/WalletMappingProfile.cs
@@ -4,17 +4,31 @@
     using Data.Entities.Blockchain.Wallets;
     using Models.Requests.BlockchainRequests.WalletModels;
     using Models.Responses.BlockchainResponses.WalletModels;
+    using System.Globalization;
 
     public class WalletMappingProfile : Profile
     {
         public WalletMappingProfile()
         {
             this.CreateMap<CreateWalletModel, Wallet>()
-                .ForMember(e => e.NormalizedTag, m => m.MapFrom(m => m.Address.ToUpper()));
-            this.CreateMap<Wallet, CreatedWalletModel>();
-            this.CreateMap<Wallet, EditedWalletModel>();
-            this.CreateMap<Wallet, DeletedWalletModel>();
-            this.CreateMap<Wallet, WalletListingModel>();
+                .ForMember(e => e.NormalizedTag, m => m.MapFrom(m => m.Address.ToUpper()))
+                .ForMember(e => e.Owner, m => m.Ignore())
+                .ForMember(e => e.Provider, m => m.Ignore());
+            this.CreateMap<Wallet, CreatedWalletModel>()
+                .ForMember(m => m.Provider, e => e.MapFrom(e => e.Provider.Name))
+                .ForMember(m => m.Balance, e => e.MapFrom(e => e.Balance.ToString("F", CultureInfo.InvariantCulture) + " AVAX"))
+                .ForMember(m => m.Owner, e => e.MapFrom(e => e.Owner.UserName));
+            this.CreateMap<Wallet, EditedWalletModel>()
+                .ForMember(m => m.Provider, e => e.MapFrom(e => e.Provider.Name))
+                .ForMember(m => m.Balance, e => e.MapFrom(e => e.Balance.ToString("F", CultureInfo.InvariantCulture) + " AVAX"))
+                .ForMember(m => m.Owner, e => e.MapFrom(e => e.Owner.UserName));
+            this.CreateMap<Wallet, DeletedWalletModel>()
+                .ForMember(m => m.Provider, e => e.MapFrom(e => e.Provider.Name))
+                .ForMember(m => m.Owner, e => e.MapFrom(e => e.Owner.UserName));
+            this.CreateMap<Wallet, WalletListingModel>()
+                .ForMember(m => m.Provider, e => e.MapFrom(e => e.Provider.Name))
+                .ForMember(m => m.Balance, e => e.MapFrom(e => e.Balance.ToString("F", CultureInfo.InvariantCulture) + " AVAX"))
+                .ForMember(m => m.Owner, e => e.MapFrom(e => e.Owner.UserName));
         }
     }
 }
