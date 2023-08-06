5505c8a30135604f257c85af1a862bc24f5047fa
MilenDinev <dinev.milen@gmail.com>
2023-08-06 21:39:27 +0300

    Initial creation and configuration of database relation between Profile and Erc721aUnit

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Profile/ProfileErc721aUnitConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace JaxWorld.Data.Configurations.Profile
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Data.Entities.Blockchain.Units;
+
+    public class ProfileErc721aUnitConfig : IEntityTypeConfiguration<Erc721aUnit>
+    {
+        public void Configure(EntityTypeBuilder<Erc721aUnit> builder)
+        {
+            builder.HasOne(s => s.Profile)
+            .WithMany(u => u.Erc721aUnits)
+            .HasForeignKey(s => s.ProfileId)
+            .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
