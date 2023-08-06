9c463f8929855e7c6fdfe78c77b2ea7393951e0f
MilenDinev <dinev.milen@gmail.com>
2023-08-06 21:59:02 +0300

    making ContractId nullable

--- Web/JaxWorldPlatform/JaxWorld.Data/Interfaces/IEntities/IBlockchain/ITransactions/ITransaction.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Interfaces/IEntities/IBlockchain/ITransactions/ITransaction.cs
@@ -5,7 +5,7 @@
         string TxnHash { get; set; }
         DateTime CreationDate { get; set; }
         int StateId { get; set; }
-        int ContractId { get; set; }
+        int? ContractId { get; set; }
         int NetworkId { get; set; }
     }
 }
