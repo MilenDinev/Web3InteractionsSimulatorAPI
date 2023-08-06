b78dbf541cd7fe3197e9661736005d764d97560a
MilenDinev <dinev.milen@gmail.com>
2023-08-06 21:53:05 +0300

    Adding Erc721AUnits property and Precision attribute to Value property

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Properties/Utility.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Properties/Utility.cs
@@ -1,10 +1,19 @@
 ﻿namespace JaxWorld.Data.Entities.Blockchain.Properties
 {
     using Base;
+    using Data.Entities.Blockchain.Units;
     using Interfaces.IEntities.IBlockchain.IProperties;
+    using Microsoft.EntityFrameworkCore;
 
     public class Utility : Property, IUtility
     {
+        public Utility()
+        {
+            Erc721AUnits = new HashSet<Erc721aUnit>();
+        }
+        public string DisplayType { get; set; }
+        [Precision(18, 2)]
         public decimal Value { get; set; }
+        public virtual ICollection<Erc721aUnit> Erc721AUnits { get; set; }
     }
 }
