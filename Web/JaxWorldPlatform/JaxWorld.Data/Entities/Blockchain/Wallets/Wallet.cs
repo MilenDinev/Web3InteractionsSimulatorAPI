8071c1b1643c925b2ea723040412a2790958a791
MilenDinev <dinev.milen@gmail.com>
2023-08-06 21:55:50 +0300

    Renaming Chains to Networks and adding Precision Attribute to Balance property

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Wallets/Wallet.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Blockchain/Wallets/Wallet.cs
@@ -5,25 +5,27 @@
     using Transactions;
     using Entities.Blockchain.Units;
     using Interfaces.IEntities.IBlockchain.IWallets;
+    using Microsoft.EntityFrameworkCore;
 
     public class Wallet : Entity, IWallet
     {
         public Wallet()
         {
-            Chains = new HashSet<Network>();
+            Networks = new HashSet<Network>();
             Transactions = new HashSet<Transaction>();
             CreatedContracts = new HashSet<Contract>();
             Erc721aUnits = new HashSet<Erc721aUnit>();
         }
 
         public string Address { get; set; }
+        [Precision(18, 2)]
         public decimal Balance { get; set; }
         public int OwnerId { get; set; }
         public virtual User Owner { get; set; }
         public int ProviderId { get; set; }
         public virtual Provider Provider { get; set; }
 
-        public virtual ICollection<Network> Chains { get; set; }
+        public virtual ICollection<Network> Networks { get; set; }
         public virtual ICollection<Transaction> Transactions { get; set; }
         public virtual ICollection<Contract> CreatedContracts { get; set; }
         public virtual ICollection<Erc721aUnit> Erc721aUnits { get; set; }
