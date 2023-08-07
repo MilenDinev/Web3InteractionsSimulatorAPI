2b0b579670675325ead76cff64fd2a5117272355
MilenDinev <dinev.milen@gmail.com>
2023-08-08 00:01:08 +0300

    Adding validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/EditProfileModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/EditProfileModel.cs
@@ -1,9 +1,19 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.ProfileModels
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class EditProfileModel
     {
+        [MinLength(2, ErrorMessage = "Name must be between 2 and 10 symbols!")]
+        [MaxLength(15, ErrorMessage = "Name must be between 2 and 10 symbols!")]
         public string Name { get; set; }
+
+        [MinLength(1, ErrorMessage = "Symbol must be between 1 and 5 symbols!")]
+        [MaxLength(5, ErrorMessage = "Symbol must be between 1 and 5 symbols!")]
         public string Symbol { get; set; }
+
+        [MinLength(10, ErrorMessage = "Description must be between 10 and 50 symbols!")]
+        [MaxLength(50, ErrorMessage = "Description must be between 10 and 50 symbols!")]
         public string Description { get; set; }
         public int? TotalSupply { get; set; }
     }
