09a451710244054a3275dabeef985c9589eab6d1
MilenDinev <dinev.milen@gmail.com>
2023-08-08 00:03:53 +0300

    Refactoring validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/CreateProfileModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/CreateProfileModel.cs
@@ -4,19 +4,19 @@
 
     public class CreateProfileModel
     {
-        [Required(ErrorMessage = "Profile name is required and must be between 21 and 15 symbols!")]
-        [MaxLength(15, ErrorMessage = "Profile name is required and must be between 2 and 10 symbols!")]
-        [MinLength(2, ErrorMessage = "Profile name is required and must be between 2 and 10 symbols!")]
+        [Required(ErrorMessage = "Name is required!")]
+        [MinLength(2, ErrorMessage = "Name must be between 2 and 10 symbols!")]
+        [MaxLength(15, ErrorMessage = "Name must be between 2 and 10 symbols!")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Profile symbol is required and must be between 1 and 5 symbols!")]
-        [MaxLength(5, ErrorMessage = "Profile symbol is required and must be between 1 and 5 symbols!")]
-        [MinLength(1, ErrorMessage = "Profile symbol is required and must be between 1 and 5 symbols!")]
+        [Required(ErrorMessage = "Symbol is required!")]
+        [MinLength(1, ErrorMessage = "Symbol must be between 1 and 5 symbols!")]
+        [MaxLength(5, ErrorMessage = "Symbol must be between 1 and 5 symbols!")]
         public string Symbol { get; set; }
 
-        [Required(ErrorMessage = "Profile description is required and must be between 10 and 50 symbols!")]
-        [MaxLength(50, ErrorMessage = "Profile description is required and must be between 10 and 50 symbols!")]
-        [MinLength(10, ErrorMessage = "Profile description is required and must be between 10 and 50 symbols!")]
+        [Required(ErrorMessage = "Description is required!")]
+        [MinLength(10, ErrorMessage = "Description must be between 10 and 50 symbols!")]
+        [MaxLength(50, ErrorMessage = "Description must be between 10 and 50 symbols!")]
         public string Description { get; set; }
         public int TotalSupply { get; set; }
         public int StandardId { get; set; }
