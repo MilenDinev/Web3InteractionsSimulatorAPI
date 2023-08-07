57b75eebc1bad48053570820410594bf03841513
MilenDinev <dinev.milen@gmail.com>
2023-08-08 00:04:06 +0300

    Adding validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/WalletModels/CreateWalletModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/WalletModels/CreateWalletModel.cs
@@ -8,9 +8,9 @@
         [MaxLength(50, ErrorMessage = "Wallet address is required and must be between 5 and 50 symbols!")]
         [MinLength(5, ErrorMessage = "Wallet address is required and must be between 5 and 50 symbols!")]
         public string Address { get; set; }
-        [Required(ErrorMessage = "Provider is required! Select between '1\' or \'metamask\' for metamask, \'2\' or \'coinbase\' for coinbase, \'3\' or \'walletconnect\' for walletconnect.")]
+        [Required(ErrorMessage = "Provider is required!")]
         [RegularExpression("(^(?i)metamask|^coinbase|^walletconnect|^1$|^2$|^3$)",
-            ErrorMessage = "Typed role is not valid! Please type between \'1\' or \'metamask\' for metamask, \'2\' or \'coinbase\' for coinbase, \'3\' or \'walletconnect\' for walletconnect.")]
+            ErrorMessage = "Provider is not valid! Please type between \'1\' or \'metamask\' for metamask, \'2\' or \'coinbase\' for coinbase, \'3\' or \'walletconnect\' for walletconnect.")]
         public string Provider { get; set; }
     }
 }
