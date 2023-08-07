a37e4f9c628aa0a993e606b784771026a8a6bc2b
MilenDinev <dinev.milen@gmail.com>
2023-08-08 00:04:51 +0300

    Adding validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/Base/EditPropertyModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/Base/EditPropertyModel.cs
@@ -1,9 +1,12 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.PropertiesModels.Base
 {
     using Interfaces;
+    using System.ComponentModel.DataAnnotations;
 
     public abstract class EditPropertyModel : IEditPropertyModel
     {
-        public string Name { get; set; }
+        [MaxLength(15, ErrorMessage = "TraitType must be between 2 and 15 symbols!")]
+        [MinLength(2, ErrorMessage = "TraitType must be between 2 and 15 symbols!")]
+        public string TraitType { get; set; }
     }
 }
