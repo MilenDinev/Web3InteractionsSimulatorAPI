3276e608b6646b7b122f1c163ec6a1147f972cfd
MilenDinev <dinev.milen@gmail.com>
2023-08-08 00:01:58 +0300

    Adding validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/UnitModels/EditErc721aUnitModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/UnitModels/EditErc721aUnitModel.cs
@@ -1,8 +1,16 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.UnitModels
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class EditErc721aUnitModel
     {
+        [Required(ErrorMessage = "Name is required!")]
+        [MinLength(2, ErrorMessage = "Name must be between 2 and 15 symbols!")]
+        [MaxLength(15, ErrorMessage = "Name must be between 2 and 15 symbols!")]
         public string Name { get; set; }
-        public string DNA { get; set; }
+
+        [MinLength(5, ErrorMessage = "DNA must be between 5 and 25 symbols!")]
+        [MaxLength(25, ErrorMessage = "DNA must be between 5 and 25 symbols!")]
+        public string? DNA { get; set; }
     }
 }
