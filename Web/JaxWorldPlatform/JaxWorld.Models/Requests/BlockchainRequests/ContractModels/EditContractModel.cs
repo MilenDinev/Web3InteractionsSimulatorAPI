d58b2fbade29f01d4018cd485e2acf7cb0faad09
MilenDinev <dinev.milen@gmail.com>
2023-08-08 00:00:22 +0300

    Refactoring validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/EditContractModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/EditContractModel.cs
@@ -4,9 +4,9 @@
 
     public class EditContractModel
     {
-        [Required(ErrorMessage = "Contract name is required and must be between 2 and 15 symbols!")]
-        [MaxLength(15, ErrorMessage = "Contract name is required and must be between 2 and 15 symbols!")]
-        [MinLength(2, ErrorMessage = "Contract name is required and must be between 2 and 15 symbols!")]
+        [Required(ErrorMessage = "Contract name is required")]
+        [MaxLength(15, ErrorMessage = "Contract must be between 2 and 15 symbols!")]
+        [MinLength(2, ErrorMessage = "Contract must be between 2 and 15 symbols!")]
         public string Name { get; set; }
     }
 }
