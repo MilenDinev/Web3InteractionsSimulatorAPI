8a51012501d5fe5dc6ced874db40287500445654
MilenDinev <dinev.milen@gmail.com>
2023-08-08 00:01:41 +0300

    Adding validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/EditUtilityModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/EditUtilityModel.cs
@@ -1,9 +1,13 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.PropertiesModels
 {
     using Base;
+    using System.ComponentModel.DataAnnotations;
 
     public class EditUtilityModel : EditPropertyModel
     {
+        [MinLength(2, ErrorMessage = "DisplayType must be between 2 and 10 symbols!")]
+        [MaxLength(10, ErrorMessage = "DisplayType must be between 2 and 10 symbols!")]
+        public string DisplayType { get; set; }
         public decimal Value { get; set; }
     }
 }
