fc005855c9fbbe514f102c641f4814a2215c5f04
MilenDinev <dinev.milen@gmail.com>
2023-08-08 00:00:38 +0300

    Refactoring validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/NetworkModels/CreateNetworkModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/NetworkModels/CreateNetworkModel.cs
@@ -4,21 +4,24 @@
 
     public class CreateNetworkModel
     {
-        [Required(ErrorMessage = "Network name is required and must be between 2 and 25 symbols!")]
-        [MaxLength(25, ErrorMessage = "Network name is required and must be between 2 and 25 symbols!")]
-        [MinLength(2, ErrorMessage = "Network name is required and must be between 2 and 25 symbols!")]
+        [Required(ErrorMessage = "Name is required!")]
+        [MinLength(2, ErrorMessage = "Name must be between 2 and 25 symbols!")]
+        [MaxLength(25, ErrorMessage = "Name must be between 2 and 25 symbols!")]
         public string Name { get; init; }
 
-        [Required(ErrorMessage = "Network symbol is required and must be between 1 and 10 symbols!")]
-        [MaxLength(10, ErrorMessage = "Network symbol is required and must be between 1 and 10 symbols!")]
-        [MinLength(1, ErrorMessage = "Network symbol is required and must be between 1 and 10 symbols!")]
+        [Required(ErrorMessage = "Symbol is required!")]
+        [MinLength(1, ErrorMessage = "Symbol must be between 1 and 10 symbols!")]
+        [MaxLength(10, ErrorMessage = "Symbol must be between 1 and 10 symbols!")]
         public string Symbol { get; init; }
 
-        [Required(ErrorMessage = "Network rpcUrl is required and must be between 5 and 35 symbols!")]
-        [MaxLength(35, ErrorMessage = "Network symbol is required and must be between 5 and 35 symbols!")]
-        [MinLength(5, ErrorMessage = "Network symbol is required and must be between 5 and 35 symbols!")]
+        [Required(ErrorMessage = "RpcUrl is required!")]
+        [MinLength(5, ErrorMessage = "RpcUrl must be between 5 and 35 symbols!")]
+        [MaxLength(35, ErrorMessage = "RpcUrl must be between 5 and 35 symbols!")]
         public string RpcUrl { get; init; }
         public int ChainId { get; init; }
+
+        [MinLength(5, ErrorMessage = "ExplorerUrl must be between 5 and 35 symbols!")]
+        [MaxLength(35, ErrorMessage = "ExplorerUrl must be between 5 and 35 symbols!")]
         public string? ExplorerUrl { get; init; }
     }
 }
