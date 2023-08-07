d349f3db84e4857c6b60e281367fd130646fcb82
MilenDinev <dinev.milen@gmail.com>
2023-08-08 00:01:23 +0300

    Adding validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/CreateUtilityModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/CreateUtilityModel.cs
@@ -1,9 +1,13 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.PropertiesModels
 {
     using Base;
+    using System.ComponentModel.DataAnnotations;
 
     public class CreateUtilityModel : CreatePropertyModel
     {
+        [Required(ErrorMessage = "DisplayType is required!")]
+        [MinLength(2, ErrorMessage = "DisplayType is required and must be between 2 and 10 symbols!")]
+        [MaxLength(10, ErrorMessage = "DisplayType is required and must be between 2 and 10 symbols!")]
         public string DisplayType { get; set; }
         public decimal Value { get; set; }
     }
