4bc532c2cb20f62930986f484a7ae5443e98b546
MilenDinev <dinev.milen@gmail.com>
2023-08-08 23:36:02 +0300

    Updating validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/CreateProfileModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/CreateProfileModel.cs
@@ -1,25 +1,24 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.ProfileModels
 {
+    using Constants;
     using System.ComponentModel.DataAnnotations;
 
     public class CreateProfileModel
     {
-        [Required(ErrorMessage = "Name is required!")]
-        [MinLength(2, ErrorMessage = "Name must be between 2 and 10 symbols!")]
-        [MaxLength(15, ErrorMessage = "Name must be between 2 and 10 symbols!")]
-        public string Name { get; set; }
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [StringLength(20, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 5)]
+        public string? Name { get; set; }
 
-        [Required(ErrorMessage = "Symbol is required!")]
-        [MinLength(1, ErrorMessage = "Symbol must be between 1 and 5 symbols!")]
-        [MaxLength(5, ErrorMessage = "Symbol must be between 1 and 5 symbols!")]
-        public string Symbol { get; set; }
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [StringLength(6, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 2)]
+        public string? Symbol { get; set; }
 
-        [Required(ErrorMessage = "Description is required!")]
-        [MinLength(10, ErrorMessage = "Description must be between 10 and 50 symbols!")]
-        [MaxLength(50, ErrorMessage = "Description must be between 10 and 50 symbols!")]
-        public string Description { get; set; }
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [StringLength(100, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 10)]
+        public string? Description { get; set; }
         public int TotalSupply { get; set; }
         public int StandardId { get; set; }
         public int ContractId { get; set; }
     }
 }
+
