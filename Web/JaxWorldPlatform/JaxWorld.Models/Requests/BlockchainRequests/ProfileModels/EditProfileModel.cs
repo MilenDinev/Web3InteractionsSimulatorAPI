52003499f9d52fadf1f8e3467f835ffaa59c8ce3
MilenDinev <dinev.milen@gmail.com>
2023-08-08 21:19:24 +0300

    Making Name, Symbol and Description properties nullable

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/EditProfileModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/EditProfileModel.cs
@@ -6,15 +6,15 @@
     {
         [MinLength(2, ErrorMessage = "Name must be between 2 and 10 symbols!")]
         [MaxLength(15, ErrorMessage = "Name must be between 2 and 10 symbols!")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [MinLength(1, ErrorMessage = "Symbol must be between 1 and 5 symbols!")]
         [MaxLength(5, ErrorMessage = "Symbol must be between 1 and 5 symbols!")]
-        public string Symbol { get; set; }
+        public string? Symbol { get; set; }
 
         [MinLength(10, ErrorMessage = "Description must be between 10 and 50 symbols!")]
         [MaxLength(50, ErrorMessage = "Description must be between 10 and 50 symbols!")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public int? TotalSupply { get; set; }
     }
 }
