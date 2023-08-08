a048b224b3d567c2e3e609c55ae54b6c55eef4b3
MilenDinev <dinev.milen@gmail.com>
2023-08-08 23:35:45 +0300

    Updating validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/CreateContractModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/CreateContractModel.cs
@@ -1,21 +1,20 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.ContractModels
 {
+    using Constants;
     using System.ComponentModel.DataAnnotations;
 
     public class CreateContractModel
     {
-        [Required(ErrorMessage = "Name!")]
-        [MinLength(2, ErrorMessage = "Name must be between 2 and 15 symbols!")]
-        [MaxLength(15, ErrorMessage = "Name must be between 2 and 15 symbols!")]
-        public string Name { get; set; }
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [StringLength(15, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 2)]
+        public string? Name { get; set; }
 
-        [Required(ErrorMessage = "Address is required!")]
-        [MinLength(5, ErrorMessage = "Address must be atleast 5 symbols!")]
-        public string Address { get; set; }
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [MinLength(5, ErrorMessage = ValidationMessages.MinLength)]
+        public string? Address { get; set; }
 
-        [Required(ErrorMessage = "Network is required!")]
         [RegularExpression("(^(?i)1$|^2$|^3$)",
-            ErrorMessage = "Network is not valid! Please type between \'1\' for Avalanche Mainnet Network and \'2\' for Avalanche FUJI C-Chain.")]
+            ErrorMessage = ValidationMessages.Network)]
         public int NetworkId { get; set; }
     }
 }
