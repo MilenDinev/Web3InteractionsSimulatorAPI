80668d9f404171bc1eca543fc456174fee5599cf
MilenDinev <dinev.milen@gmail.com>
2023-08-08 21:24:50 +0300

    Edit Validation Properties

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/EntityRequests/CreateUserModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/EntityRequests/CreateUserModel.cs
@@ -4,16 +4,15 @@
 
     public class CreateUserModel
     {
-        [Required(ErrorMessage = "UserName is required!")]
+        [Required(ErrorMessage = "Username is required!")]
         public string UserName { get; set; }
         public string? WalletAddress { get; set; }
-
+        [Required(ErrorMessage = "Email is required!")]
+        [EmailAddress(ErrorMessage = "Please provide valid email address!")]
+        public string Email { get; set; }
         [Required(ErrorMessage = "Password is required!")]
         public string Password { get; set; }
     }
 }
 
 
-//[Required(ErrorMessage = "Email is required!")]
-//[EmailAddress(ErrorMessage = "Please provide valid email address!")]
-//public string Email { get; set; }
