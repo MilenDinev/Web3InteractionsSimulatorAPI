4bd346ef60b2a4952fcef428d43beacf20da96aa
MilenDinev <dinev.milen@gmail.com>
2023-08-08 21:23:46 +0300

    Edit Validation Properties

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/Base/CreatePropertyModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/Base/CreatePropertyModel.cs
@@ -6,8 +6,8 @@
     public abstract class CreatePropertyModel : ICreatePropertyModel
     {
         [Required(ErrorMessage = "TraitType is required!")]
-        [MaxLength(15, ErrorMessage = "TraitType must be between 2 and 15 symbols!")]
         [MinLength(2, ErrorMessage = "TraitType must be between 2 and 15 symbols!")]
+        [MaxLength(15, ErrorMessage = "TraitType must be between 2 and 15 symbols!")]
         public string TraitType { get; set; }
     }
 }
