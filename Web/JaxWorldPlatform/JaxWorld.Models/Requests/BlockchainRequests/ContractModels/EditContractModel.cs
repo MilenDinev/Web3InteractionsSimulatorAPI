ab201aa03862969f6ebc13673ca47528b78bf925
MilenDinev <dinev.milen@gmail.com>
2023-08-08 23:35:50 +0300

    Updating validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/EditContractModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/EditContractModel.cs
@@ -1,11 +1,11 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.ContractModels
 {
     using System.ComponentModel.DataAnnotations;
+    using Constants;
 
     public class EditContractModel
     {
-        [MinLength(2, ErrorMessage = "Contract must be between 2 and 15 symbols!")]
-        [MaxLength(15, ErrorMessage = "Contract must be between 2 and 15 symbols!")]
+        [StringLength(15, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 2)]
         public string? Name { get; set; }
     }
 }
