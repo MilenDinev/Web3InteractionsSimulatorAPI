7b1e445e4f4a02d9d62f58f40bf34604fe9e1a2a
MilenDinev <dinev.milen@gmail.com>
2023-08-08 21:24:11 +0300

    Edit Validation Properties and making Name property nullable

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/UnitModels/EditErc721aUnitModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/UnitModels/EditErc721aUnitModel.cs
@@ -4,13 +4,12 @@
 
     public class EditErc721aUnitModel
     {
-        [Required(ErrorMessage = "Name is required!")]
         [MinLength(2, ErrorMessage = "Name must be between 2 and 15 symbols!")]
         [MaxLength(15, ErrorMessage = "Name must be between 2 and 15 symbols!")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        [MinLength(5, ErrorMessage = "DNA must be between 5 and 25 symbols!")]
-        [MaxLength(25, ErrorMessage = "DNA must be between 5 and 25 symbols!")]
+        [MinLength(5, ErrorMessage = "DNA must be between 5 and 45 symbols!")]
+        [MaxLength(45, ErrorMessage = "DNA must be between 5 and 45 symbols!")]
         public string? DNA { get; set; }
     }
 }
