b5c80c9ea5d69468753c4dba06ed9075abfa2f4f
MilenDinev <dinev.milen@gmail.com>
2023-08-08 21:23:52 +0300

    Edit Validation Properties

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/CreateUtilityModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/CreateUtilityModel.cs
@@ -6,8 +6,8 @@
     public class CreateUtilityModel : CreatePropertyModel
     {
         [Required(ErrorMessage = "DisplayType is required!")]
-        [MinLength(2, ErrorMessage = "DisplayType is required and must be between 2 and 10 symbols!")]
-        [MaxLength(10, ErrorMessage = "DisplayType is required and must be between 2 and 10 symbols!")]
+        [MinLength(2, ErrorMessage = "DisplayType is required and must be between 2 and 15 symbols!")]
+        [MaxLength(15, ErrorMessage = "DisplayType is required and must be between 2 and 15 symbols!")]
         public string DisplayType { get; set; }
         public decimal Value { get; set; }
     }
