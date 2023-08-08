94beab2527b62c2b7f05ad809255f636b364651c
MilenDinev <dinev.milen@gmail.com>
2023-08-08 21:18:25 +0300

    Making Name, Symbol and RpcUrl properties nullable

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/NetworkModels/EditNetworkModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/NetworkModels/EditNetworkModel.cs
@@ -6,16 +6,16 @@
     {
         [MinLength(2, ErrorMessage = "Name must be between 2 and 25 symbols!")]
         [MaxLength(25, ErrorMessage = "Name must be between 2 and 25 symbols!")]
-        public string Name { get; init; }
+        public string? Name { get; init; }
 
         [MinLength(1, ErrorMessage = "Symbol must be between 1 and 10 symbols!")]
         [MaxLength(10, ErrorMessage = "Symbol must be between 1 and 10 symbols!")]
-        public string Symbol { get; init; }
+        public string? Symbol { get; init; }
         public int? ChainId { get; init; }
 
         [MinLength(5, ErrorMessage = "RpcUrl must be between 5 and 35 symbols!")]
         [MaxLength(35, ErrorMessage = "RpcUrl must be between 5 and 35 symbols!")]
-        public string RpcUrl { get; init; }
+        public string? RpcUrl { get; init; }
 
         [MinLength(5, ErrorMessage = "ExplorerUrl must be between 5 and 35 symbols!")]
         [MaxLength(35, ErrorMessage = "ExplorerUrl must be between 5 and 35 symbols!")]
