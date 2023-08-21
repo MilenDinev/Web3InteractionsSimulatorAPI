69f3f5fca0c54434a6746d93a4050bd64dfe4a86
MilenDinev <dinev.milen@gmail.com>
2023-08-21 23:16:07 +0300

    Updating attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/EditAttributeModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/EditAttributeModel.cs
@@ -1,12 +1,12 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.PropertiesModels
 {
-    using Base;
     using System.ComponentModel.DataAnnotations;
+    using Base;
+    using Constants;
 
     public class EditAttributeModel : EditPropertyModel
     {
-        [MinLength(2, ErrorMessage = "Value must be between 2 and 15 symbols!")]
-        [MaxLength(15, ErrorMessage = "Value must be between 2 and 15 symbols!")]
-        public string Value { get; set; }
+        [StringLength(15, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 3)]
+        public string? Value { get; set; }
     }
 }
