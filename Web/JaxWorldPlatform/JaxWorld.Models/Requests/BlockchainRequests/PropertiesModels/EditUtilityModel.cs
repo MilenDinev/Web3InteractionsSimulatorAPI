128d4faea2c122281eca160fe417a346dd88f13f
MilenDinev <dinev.milen@gmail.com>
2023-08-21 23:16:34 +0300

    Updating attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/EditUtilityModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/PropertiesModels/EditUtilityModel.cs
@@ -1,12 +1,12 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.PropertiesModels
 {
     using Base;
+    using JaxWorld.Models.Constants;
     using System.ComponentModel.DataAnnotations;
 
     public class EditUtilityModel : EditPropertyModel
     {
-        [MinLength(2, ErrorMessage = "DisplayType must be between 2 and 15 symbols!")]
-        [MaxLength(15, ErrorMessage = "DisplayType must be between 2 and 15 symbols!")]
+        [StringLength(15, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 3)]
         public string? DisplayType { get; set; }
         public decimal? Value { get; set; }
     }
