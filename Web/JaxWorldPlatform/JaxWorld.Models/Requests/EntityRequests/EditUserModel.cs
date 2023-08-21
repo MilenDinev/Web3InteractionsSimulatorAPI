12c8c53815d945ddc431ae9aa559a020bb3e2b16
MilenDinev <dinev.milen@gmail.com>
2023-08-21 23:18:54 +0300

    Updating attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/EntityRequests/EditUserModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/EntityRequests/EditUserModel.cs
@@ -1,14 +1,20 @@
 ﻿namespace JaxWorld.Models.Requests.EntityRequests
 {
     using System.ComponentModel.DataAnnotations;
+    using Constants;
 
     public class EditUserModel
     {
+        [StringLength(25, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 4)]
         public string? UserName { get; set; }
+
+        [StringLength(50, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 5)]
         public string? WalletAddress { get; set; }
-        [EmailAddress(ErrorMessage = "Please provide valid email address!")]
+
+        [EmailAddress(ErrorMessage = ValidationMessages.Email)]
         public string? Email { get; set; }
         public string? Password { get; set; }
         public string? ConfirmPassword { get; set; }
     }
 }
+
