dcd4a39f5358d769f38456a39ceaaffb46df4dc6
MilenDinev <dinev.milen@gmail.com>
2023-08-21 23:17:20 +0300

    Updating attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/WalletModels/CreateWalletModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/WalletModels/CreateWalletModel.cs
@@ -1,16 +1,17 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.WalletModels
 {
     using System.ComponentModel.DataAnnotations;
+    using Constants;
 
     public class CreateWalletModel
     {
-        [Required(ErrorMessage = "Wallet address is required and must be between 5 and 50 symbols!")]
-        [MinLength(5, ErrorMessage = "Wallet address is required and must be between 5 and 50 symbols!")]
-        [MaxLength(50, ErrorMessage = "Wallet address is required and must be between 5 and 50 symbols!")]
-        public string Address { get; set; }
-        [Required(ErrorMessage = "Provider is required!")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [StringLength(50, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 5)]
+        public string? Address { get; set; }
+        [Required(ErrorMessage = ValidationMessages.Required)]
         [RegularExpression("(^(?i)metamask|^coinbase|^walletconnect|^1$|^2$|^3$)",
-            ErrorMessage = "Provider is not valid! Please type between \'1\' or \'metamask\' for metamask, \'2\' or \'coinbase\' for coinbase, \'3\' or \'walletconnect\' for walletconnect.")]
-        public string Provider { get; set; }
+            ErrorMessage = ValidationMessages.Provider)]
+        public string? Provider { get; set; }
     }
 }
+
