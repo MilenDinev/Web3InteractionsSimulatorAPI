9f7d77bb9d81891143bf360d69660107cc75124b
MilenDinev <dinev.milen@gmail.com>
2023-08-21 23:17:49 +0300

    Updating attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/EntityRequests/CreateUserModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/EntityRequests/CreateUserModel.cs
@@ -1,18 +1,23 @@
 ﻿namespace JaxWorld.Models.Requests.EntityRequests
 {
     using System.ComponentModel.DataAnnotations;
+    using Constants;
 
     public class CreateUserModel
     {
-        [Required(ErrorMessage = "Username is required!")]
-        public string UserName { get; set; }
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [StringLength(25, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 4)]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [StringLength(50, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 5)]
         public string? WalletAddress { get; set; }
-        [Required(ErrorMessage = "Email is required!")]
-        [EmailAddress(ErrorMessage = "Please provide valid email address!")]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "Password is required!")]
-        public string Password { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [EmailAddress(ErrorMessage = ValidationMessages.Email)]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string? Password { get; set; }
     }
 }
-
-
