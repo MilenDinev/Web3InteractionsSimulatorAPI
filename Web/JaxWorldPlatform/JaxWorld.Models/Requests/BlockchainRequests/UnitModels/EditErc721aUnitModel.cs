73cbad4d7c36ed1ca9c90ce6809820d8833fd66c
MilenDinev <dinev.milen@gmail.com>
2023-08-21 23:16:58 +0300

    Updating attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/UnitModels/EditErc721aUnitModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/UnitModels/EditErc721aUnitModel.cs
@@ -1,15 +1,14 @@
 ﻿namespace JaxWorld.Models.Requests.BlockchainRequests.UnitModels
 {
     using System.ComponentModel.DataAnnotations;
+    using Constants;
 
     public class EditErc721aUnitModel
     {
-        [MinLength(2, ErrorMessage = "Name must be between 2 and 15 symbols!")]
-        [MaxLength(15, ErrorMessage = "Name must be between 2 and 15 symbols!")]
+        [StringLength(15, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 2)]
         public string? Name { get; set; }
 
-        [MinLength(5, ErrorMessage = "DNA must be between 5 and 45 symbols!")]
-        [MaxLength(45, ErrorMessage = "DNA must be between 5 and 45 symbols!")]
+        [StringLength(30, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 5)]
         public string? DNA { get; set; }
     }
 }
