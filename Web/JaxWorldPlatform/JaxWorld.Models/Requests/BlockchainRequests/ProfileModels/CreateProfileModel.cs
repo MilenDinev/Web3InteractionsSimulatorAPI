f4dcb90c230a9d29fa2645c8e39e6cdb56be926e
MilenDinev <dinev.milen@gmail.com>
2023-08-23 02:48:54 +0300

    making properties non nullable

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/CreateProfileModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ProfileModels/CreateProfileModel.cs
@@ -7,15 +7,15 @@
     {
         [Required(ErrorMessage = ValidationMessages.Required)]
         [StringLength(20, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 5)]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         [Required(ErrorMessage = ValidationMessages.Required)]
         [StringLength(6, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 2)]
-        public string? Symbol { get; set; }
+        public string Symbol { get; set; }
 
         [Required(ErrorMessage = ValidationMessages.Required)]
         [StringLength(100, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 10)]
-        public string? Description { get; set; }
+        public string Description { get; set; }
         public int TotalSupply { get; set; }
         public int StandardId { get; set; }
         public int ContractId { get; set; }
