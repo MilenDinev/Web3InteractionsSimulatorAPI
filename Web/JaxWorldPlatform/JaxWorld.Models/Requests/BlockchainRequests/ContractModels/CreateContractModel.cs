11d3ec961e675446f03a551767d5c0195c8d2027
MilenDinev <dinev.milen@gmail.com>
2023-08-23 02:47:41 +0300

    Updating validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/CreateContractModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/CreateContractModel.cs
@@ -7,11 +7,19 @@
     {
         [Required(ErrorMessage = ValidationMessages.Required)]
         [StringLength(15, ErrorMessage = ValidationMessages.MinMaxLength, MinimumLength = 2)]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         [Required(ErrorMessage = ValidationMessages.Required)]
         [MinLength(5, ErrorMessage = ValidationMessages.MinLength)]
-        public string? Address { get; set; }
+        public string ContractAddress { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [MinLength(5, ErrorMessage = ValidationMessages.MinLength)]
+        public string CreatorAddress { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [MinLength(5, ErrorMessage = ValidationMessages.MinLength)]
+        public string CreationTxnHash { get; set; }
 
         [RegularExpression("(^(?i)1$|^2$|^3$)",
             ErrorMessage = ValidationMessages.Network)]
