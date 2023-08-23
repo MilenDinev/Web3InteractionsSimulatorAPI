fe2f836067c537f45d6ad22012c1dd54f590e93b
MilenDinev <dinev.milen@gmail.com>
2023-08-23 15:27:54 +0300

    Renaming TxnOperation to TnxAction

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Transactions/TxnAction.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Transactions/TxnAction.cs
@@ -3,9 +3,8 @@
     using Wallets;
     using Profiles;
 
-    public class TxnOperation
+    public class TxnAction : Entity
     {
-        public int Id { get; set; }
         public string Operation { get; set; }
         public int ProfileId { get; set; }
         public virtual Profile Profile { get; set; }
