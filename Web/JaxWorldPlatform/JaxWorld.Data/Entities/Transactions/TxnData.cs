484f21767a670e248d4dba89e125cacd512351ba
MilenDinev <dinev.milen@gmail.com>
2023-08-23 15:29:04 +0300

    Inherits Entity and refactoring

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Transactions/TxnData.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Transactions/TxnData.cs
@@ -1,6 +1,14 @@
 ﻿namespace JaxWorld.Data.Entities.Transactions
 {
-    public class TxnData
+    using Microsoft.EntityFrameworkCore;
+
+    public class TxnData : Entity
     {
+        public ulong? StartTokenId { get; set; }
+        public ulong? QuantityClaimed { get; set; }
+        [Precision(18, 2)]
+        public decimal? Value { get; set; }
+        public string prevURI { get; set; }
+        public string newURI { get; set; }
     }
 }
