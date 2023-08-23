daa0e96f40cf6b349e959a7eb159547e942712d8
MilenDinev <dinev.milen@gmail.com>
2023-08-23 14:11:43 +0300

    Temp comments added

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Entity.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Entity.cs
@@ -10,8 +10,10 @@
         public int Id { get; set; }
         public string NormalizedTag { get; set; }
         public int CreatorId { get; set; }
+        //public virtual User Creator { get; set; }
         public DateTime CreationDate { get; set; }
         public int LastModifierId { get; set; }
+        //public virtual User LastModifier { get; set; }
         public DateTime LastModificationDate { get; set; }
         public bool Deleted { get; set; }
     }
