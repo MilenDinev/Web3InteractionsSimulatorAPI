b0fced52a0cb611e783b2fa2890d7bb210d9aeb4
MilenDinev <dinev.milen@gmail.com>
2023-08-23 15:42:09 +0300

    Deleting Standard Property

--- Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/ContractModels/ContractListingModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/ContractModels/ContractListingModel.cs
@@ -6,7 +6,6 @@
         public string Address { get; set; }
         public string Balance { get; set; }
         public string EstimatedValue { get; set; }
-        public string Standard { get; set; }
         public string CreatorAddress { get; set; }
     }
 }
