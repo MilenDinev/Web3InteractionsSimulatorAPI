e0b723aacfa1aae4a546f3a2246cd411f49b1e78
MilenDinev <dinev.milen@gmail.com>
2023-08-23 17:51:49 +0300

    Updating Attributes Params

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Base/Property.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Base/Property.cs
@@ -1,11 +1,12 @@
 ﻿namespace JaxWorld.Data.Entities.Base
 {
     using System.ComponentModel.DataAnnotations.Schema;
+    using Constants;
     using Interfaces.IEntities.IBase;
 
     public abstract class Property : Entity, IProperty
     {
-        [Column("TraitType", Order = 2)]
+        [Column(AttributesParams.TraitType, Order = 2)]
         public string TraitType { get; set; }
     }
 }
