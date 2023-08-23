a32e5dd46ed426cae3c30a2f9d54f9cc955a2ee0
MilenDinev <dinev.milen@gmail.com>
2023-08-23 15:29:12 +0300

    Inherits Entity and refactoring

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Transactions/TxnLog.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Transactions/TxnLog.cs
@@ -1,6 +1,19 @@
 ﻿namespace JaxWorld.Data.Entities.Transactions
 {
+    using Contracts;
+
     public class TxnLog : Entity
     {
+        public TxnLog()
+        {
+            this.Topics = new HashSet<TxnTopic>();
+        }
+        public int ContractId { get; set; }
+        public virtual Contract Contract { get; set; }
+        public int TxnMethodId { get; set; }
+        public virtual TxnMethod TxnMethod { get; set; }
+        public int TxnDataId { get; set; }
+        public virtual TxnData TxnData { get; set; }
+        public virtual ICollection<TxnTopic> Topics { get; set; }
     }
 }
