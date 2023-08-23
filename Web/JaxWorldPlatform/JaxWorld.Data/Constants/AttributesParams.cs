d3d3eadb06db00dcef74976757ae94ce123e9399
MilenDinev <dinev.milen@gmail.com>
2023-08-23 17:51:03 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Constants/AttributesParams.cs
@@ -0,0 +1,11 @@
+﻿namespace JaxWorld.Data.Constants
+{
+    internal static class AttributesParams
+    {
+        internal const int DecimalPrecision = 18;
+        internal const int DecimalScale = 2;
+        internal const string TraitType = "TraitType";
+        internal const string Name = "Name";
+        internal const string Id = "Id";
+    }
+}
---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Constants/AttributesParams.cs
@@ -0,0 +1,34 @@
+﻿namespace JaxWorld.Models.Constants
+{
+    internal static class AttributesParams
+    {
+        internal const int UserNameMinLength = 4;
+        internal const int UserNameMaxLength = 15;
+        internal const int ContractNameMinLength = 2;
+        internal const int ContractNameMaxLength = 15;
+        internal const int NetworkNameMinLength = 5;
+        internal const int NetworkNameMaxLength = 25;
+        internal const int ProfileNameMinLength = 5;
+        internal const int ProfileNameMaxLength = 20;
+        internal const int UnitNameMinLength = 2;
+        internal const int UnitNameMaxLength = 15;
+        internal const int DNAMinLength = 2;
+        internal const int DNAMaxLength = 15;
+        internal const int WalletAddressMinLength = 5;
+        internal const int WalletAddressMaxLength = 50;
+        internal const int NetworkSymbolMinLength = 2;
+        internal const int NetworkSymbolMaxLength = 6;
+        internal const int ProfileSymbolMinLength = 2;
+        internal const int ProfileSymbolMaxLength = 6;
+        internal const int ProfileDescriptionMinLength = 10;
+        internal const int ProfileDescriptionMaxLength = 150;
+        internal const int TraitTypeMinLength = 3;
+        internal const int TraitTypeMaxLength = 15;
+        internal const int ValueMinLength = 3;
+        internal const int ValueMaxLength = 15;
+        internal const int DisplayTypeMinLength = 3;
+        internal const int DisplayTypeMaxLength = 15;
+        internal const int ContractAddressMinLength = 5;
+        internal const int TxnHashMinLength = 5;
+    }
+}
