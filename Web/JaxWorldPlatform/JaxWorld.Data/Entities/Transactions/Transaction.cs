92585c5952dd3aeb551cb0d706675ce7fc9db960
MilenDinev <dinev.milen@gmail.com>
2023-08-23 14:12:27 +0300

    Transaction Entity Update

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Transactions/Transaction.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Transactions/Transaction.cs
@@ -1,8 +1,7 @@
 ﻿namespace JaxWorld.Data.Entities.Transactions
 {
-    using Units;
+    using Wallets;
     using Contracts;
-    using Entities.Wallets;
     using Interfaces.IEntities.ITransactions;
 
     public class Transaction : Entity, ITransaction
@@ -10,12 +9,22 @@
         public string TxnHash { get; set; }
         public int StateId { get; set; }
         public virtual TransactionState State { get; set; }
-        public virtual Wallet Creator { get; set; }
-        public int? ContractId { get; set; }
-        public virtual Contract Contract { get; set; }
         public int NetworkId { get; set; }
-        public virtual Network Network { get; set; }
-        public int? Erc721aUnitId { get; set; }
-        public virtual Erc721aUnit Erc721aUnit { get; set; }
+        public Network Network { get; set; }
+        public int BlockId  { get; set; }
+        public Block Block  { get; set; }
+        public DateTime Timestamp  { get; set; }
+        public int InitiatorId { get; set; }
+        public virtual Wallet Initiator { get; set; }
+        public int TargetId { get; set; }
+        public virtual Contract Target { get; set; }
+        public int? OperationId { get; set; }
+        public TxnOperation Operation { get; set; }
+        public decimal Value { get; set; }
+        public decimal Fee { get; set; }
+        public decimal GasPrice { get; set; }
+        public decimal NativeValuePrice { get; set; }
+        public int Nonce { get; set; }
+        public ICollection<int> Logs { get; set; }
     }
 }
