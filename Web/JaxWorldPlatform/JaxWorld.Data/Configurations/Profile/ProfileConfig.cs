972b91c4beb54b6d891d7520e3d0db6b51afb928
MilenDinev <dinev.milen@gmail.com>
2023-08-25 23:35:57 +0300

    updating access modifier and adding explicit relationship with  usersAdding explicit relationship with  users

--- Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Profile/ProfileConfig.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Profile/ProfileConfig.cs
@@ -4,19 +4,29 @@
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
     using Entities.Profiles;
 
-    public class ProfileConfig : IEntityTypeConfiguration<Profile>
+    internal class ProfileConfig : IEntityTypeConfiguration<Profile>
     {
         public void Configure(EntityTypeBuilder<Profile> builder)
         {
             builder.HasOne(s => s.Contract)
-            .WithOne(u => u.Profile)
-            .HasForeignKey<Profile>(e => e.ContractId)
-            .OnDelete(DeleteBehavior.Restrict);
+                .WithOne(u => u.Profile)
+                .HasForeignKey<Profile>(e => e.ContractId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             builder.HasOne(s => s.Standard)
-            .WithMany(u => u.Profiles)
-            .HasForeignKey(s => s.StandardId)
-            .OnDelete(DeleteBehavior.Restrict);
+                .WithMany(u => u.Profiles)
+                .HasForeignKey(s => s.StandardId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(s => s.Creator)
+                .WithMany()
+                .HasForeignKey(s => s.CreatorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(s => s.LastModifier)
+                .WithMany()
+                .HasForeignKey(s => s.LastModifierId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
