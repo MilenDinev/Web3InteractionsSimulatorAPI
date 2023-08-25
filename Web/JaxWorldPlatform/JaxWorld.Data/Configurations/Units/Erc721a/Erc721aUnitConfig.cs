865c32176f13c20644a3db74e011e73d012bae5b
MilenDinev <dinev.milen@gmail.com>
2023-08-25 23:38:10 +0300

    Adding explicit relationship with usersAdding explicit relationship with  users

--- Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Units/Erc721a/Erc721aUnitConfig.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Units/Erc721a/Erc721aUnitConfig.cs
@@ -4,19 +4,29 @@
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
     using Entities.Units;
 
-    public class Erc721aUnitConfig : IEntityTypeConfiguration<Erc721aUnit>
+    internal class Erc721aUnitConfig : IEntityTypeConfiguration<Erc721aUnit>
     {
         public void Configure(EntityTypeBuilder<Erc721aUnit> builder)
         {
             builder.HasOne(s => s.Profile)
-            .WithMany(u => u.Erc721aUnits)
-            .HasForeignKey(s => s.ProfileId)
-            .OnDelete(DeleteBehavior.Restrict);
+                .WithMany(u => u.Erc721aUnits)
+                .HasForeignKey(s => s.ProfileId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             builder.HasOne(s => s.Holder)
-            .WithMany(u => u.Erc721aUnits)
-            .HasForeignKey(e => e.HolderId)
-            .OnDelete(DeleteBehavior.Restrict);
+                .WithMany(u => u.Erc721aUnits)
+                .HasForeignKey(e => e.HolderId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(s => s.Creator)
+                .WithMany()
+                .HasForeignKey(s => s.CreatorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(s => s.LastModifier)
+                .WithMany()
+                .HasForeignKey(s => s.LastModifierId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
