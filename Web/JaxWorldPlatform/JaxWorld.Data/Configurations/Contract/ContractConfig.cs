c773f4ed9aabc86b4c142f8c14a8d00f4be48abf
MilenDinev <dinev.milen@gmail.com>
2023-08-25 23:34:10 +0300

    Adding explicit relationship with  users

--- Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Contract/ContractConfig.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Contract/ContractConfig.cs
@@ -4,13 +4,23 @@
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
     using Entities.Contracts;
 
-    public class ContractConfig : IEntityTypeConfiguration<Contract>
+    internal class ContractConfig : IEntityTypeConfiguration<Contract>
     {
         public void Configure(EntityTypeBuilder<Contract> builder)
         {
             builder.HasOne(s => s.CreatorWallet)
                 .WithMany(u => u.CreatedContracts)
                 .HasForeignKey(s => s.CreatorWalletId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(s => s.Creator)
+                .WithMany()
+                .HasForeignKey(s => s.CreatorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(s => s.LastModifier)
+                .WithMany()
+                .HasForeignKey(s => s.LastModifierId)
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
