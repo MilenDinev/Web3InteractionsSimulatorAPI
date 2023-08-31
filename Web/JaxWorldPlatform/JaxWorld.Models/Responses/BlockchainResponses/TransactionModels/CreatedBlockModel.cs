885508a05f01ed80d2e0aac3997d282646e684d5
MilenDinev <dinev.milen@gmail.com>
2023-09-01 02:51:19 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/TransactionModels/CreatedBlockModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaxWorld.Models.Responses.BlockchainResponses.TransactionModels
+{
+    public class CreatedBlockModel
+    {
+        public int BlockId { get; set; }
+    }
+}
