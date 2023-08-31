5113aa137da4c1cb24ee8c241b7f5776af1eeb93
MilenDinev <dinev.milen@gmail.com>
2023-08-31 23:38:01 +0300

    Renaming

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Managers/Interfaces/ITransactionDeployer.cs
@@ -0,0 +1,12 @@
+﻿namespace JaxWorld.Services.Managers.Interfaces
+{
+    using Models.Requests.BlockchainRequests.TransactionModels;
+    using Models.Responses.BlockchainResponses.TransactionModels;
+
+    public interface ITransactionDeployer
+    {
+       Task<DeployedContractTransactionModel> DeployContractAsync(DeployContractTransactionModel deployContractModel, int creatorId);
+       Task<TransferedUnitTransactionModel> TransferAsync(TransferUnitTransactionModel initiateTransactionModel, int creatorId);
+       Task<ClaimedUnitTransactionModel> ClaimAsync(ClaimUnitTransactionModel initiateTransactionModel, int creatorId);
+    }
+}
