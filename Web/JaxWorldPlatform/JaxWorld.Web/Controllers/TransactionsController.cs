903cafd9d499f1c07f3c7620f9edc9f0f17d443b
MilenDinev <dinev.milen@gmail.com>
2023-08-31 23:38:50 +0300

    Deleting unused methods

--- Web/JaxWorldPlatform/JaxWorld.Web/Controllers/TransactionsController.cs
+++ Web/JaxWorldPlatform/JaxWorld.Web/Controllers/TransactionsController.cs
@@ -6,7 +6,6 @@
     using Data.Entities.Transactions;
     using Services.Main.Interfaces;
     using Services.Handlers.Interfaces;
-    using Models.Requests.BlockchainRequests.TransactionModels;
     using Models.Responses.BlockchainResponses.TransactionModels;
 
     // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -50,49 +49,6 @@
             await this.validator.ValidateEntityAsync(transaction, transactionId.ToString());
 
             return mapper.Map<TransactionListingModel>(transaction);
-        }
-
-        // POST api/<TransactionsController/Add>
-        [HttpPost("Add/")]
-        public async Task<ActionResult> Create(CreateTransactionModel transactionInput)
-        {
-            await AssignCurrentUserAsync();
-
-            var transaction = await this.finder.FindByStringOrDefaultAsync<Transaction>(transactionInput.State);
-            await this.validator.ValidateUniqueEntityAsync(transaction);
-
-            transaction = await this.transactionService.CreateAsync(transactionInput, CurrentUser.Id);
-            var createdTransaction = mapper.Map<CreatedTransactionModel>(transaction);
-
-            return CreatedAtAction(nameof(Get), "Transactions", new { id = createdTransaction.Id }, createdTransaction);
-        }
-
-        // PUT api/<TransactionsController>/5
-        [HttpPut("Edit/Transaction/{transactionId}")]
-        public async Task<ActionResult<EditedTransactionModel>> Edit(EditTransactionModel transactionInput, int transactionId)
-        {
-            await AssignCurrentUserAsync();
-
-            var transaction = await this.finder.FindByIdOrDefaultAsync<Transaction>(transactionId);
-
-            await this.validator.ValidateEntityAsync(transaction, transactionId.ToString());
-            await this.transactionService.EditAsync(transaction, transactionInput, CurrentUser.Id);
-
-            return mapper.Map<EditedTransactionModel>(transaction);
-        }
-
-        // DELETE api/<TransactionsController>/Transaction/5
-        [HttpDelete("Delete/Transaction/{transactionId}")]
-        public async Task<DeletedTransactionModel> Delete(int transactionId)
-        {
-            await AssignCurrentUserAsync();
-
-            var transaction = await this.finder.FindByIdOrDefaultAsync<Transaction>(transactionId);
-
-            await this.validator.ValidateEntityAsync(transaction, transactionId.ToString());
-            await this.transactionService.DeleteAsync(transaction, CurrentUser.Id);
-
-            return mapper.Map<DeletedTransactionModel>(transaction);
         }
     }
 }
