d7e417314b8cfc0046c43376129acfaa8b4e5e14
MilenDinev <dinev.milen@gmail.com>
2023-09-02 14:23:51 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/TransactionModels/DeployContractModel.cs
@@ -0,0 +1,9 @@
+﻿ namespace JaxWorld.Models.Requests.BlockchainRequests.TransactionModels
+{
+    public class DeployContractModel
+    {
+        public int NetworkId { get; set; }
+        public string TargetContract { get; set; }
+        public string InitiatorWallet { get; set; }
+    }
+}
