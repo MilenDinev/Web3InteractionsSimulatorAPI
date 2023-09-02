402ca315075fd99f3129e0e22657a5fd5e313763
MilenDinev <dinev.milen@gmail.com>
2023-09-02 19:09:16 +0300

    Deleting unused properties

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/CreateContractModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/CreateContractModel.cs
@@ -11,21 +11,6 @@
             MinimumLength = AttributesParams.ContractNameMinLength)]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = ValidationMessages.Required)]
-        [MinLength(AttributesParams.ContractAddressMinLength, 
-            ErrorMessage = ValidationMessages.MinLength)]
-        public string ContractAddress { get; set; }
-
-        [Required(ErrorMessage = ValidationMessages.Required)]
-        [MinLength(AttributesParams.WalletAddressMinLength, 
-            ErrorMessage = ValidationMessages.MinLength)]
-        public string CreatorAddress { get; set; }
-
-        [Required(ErrorMessage = ValidationMessages.Required)]
-        [MinLength(AttributesParams.TxnHashMinLength, 
-            ErrorMessage = ValidationMessages.MinLength)]
-        public string CreationTxnHash { get; set; }
-
         [RegularExpression("(^(?i)1$|^2$|^3$)",
             ErrorMessage = ValidationMessages.Network)]
         public int NetworkId { get; set; }
