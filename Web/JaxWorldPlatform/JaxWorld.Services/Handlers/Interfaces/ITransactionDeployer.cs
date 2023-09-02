42cc0caa6315eeeca7d9c4050aa47843db962be9
MilenDinev <dinev.milen@gmail.com>
2023-09-02 19:18:49 +0300

    Refactoring DeployContract Method

--- Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/ITransactionDeployer.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/ITransactionDeployer.cs
@@ -1,12 +1,13 @@
 ﻿namespace JaxWorld.Services.Handlers.Interfaces
 {
-    using JaxWorld.Models.Requests.BlockchainRequests.ContractModels;
+    using Data.Entities;
+    using Models.Responses.BlockchainResponses.ContractModels;
     using Models.Requests.BlockchainRequests.TransactionModels;
     using Models.Responses.BlockchainResponses.TransactionModels;
 
     public interface ITransactionDeployer
     {
-        Task<DeployedContractTransactionModel> DeployContractAsync(CreateContractModel createContractModel, int creatorId);
+        Task<DeployedContractTransactionModel> DeployContractAsync(CreatedContractModel createdContractModel, User creator);
         Task<TransferedUnitTransactionModel> TransferAsync(TransferUnitTransactionModel initiateTransactionModel, int creatorId);
         Task<ClaimedUnitTransactionModel> ClaimAsync(ClaimUnitTransactionModel initiateTransactionModel, int creatorId);
     }
--- Web/JaxWorldPlatform/JaxWorld.Services/Handlers/TransactionDeployer.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Handlers/TransactionDeployer.cs
@@ -1,67 +1,64 @@
 ﻿namespace JaxWorld.Services.Handlers
 {
-    using Interfaces;
-    using AutoMapper;
-    using Main.Interfaces;
     using System.Globalization;
+    using Data.Entities;
+    using Interfaces;
+    using Main.Interfaces;
     using Microsoft.AspNetCore.Identity;
-    using Models.Responses.BlockchainResponses.BlockModels;
-    using Models.Requests.BlockchainRequests.ContractModels;
+    using Models.Requests.BlockchainRequests.BlockModels;
+    using Models.Responses.BlockchainResponses.ContractModels;
     using Models.Requests.BlockchainRequests.TransactionModels;
     using Models.Responses.BlockchainResponses.TransactionModels;
-    using JaxWorld.Models.Requests.BlockchainRequests.BlockModels;
 
     public class TransactionDeployer : ITransactionDeployer
     {
-        private readonly IMapper mapper;
-        private readonly IContractService contractService;
         private readonly IBlockService blockService;
         private readonly ITransactionService transactionService;
 
-        public TransactionDeployer(IMapper mapper, IContractService contractService, IBlockService blockService, ITransactionService transactionService)
+        public TransactionDeployer(IBlockService blockService, ITransactionService transactionService)
         {
-            this.mapper = mapper;
-            this.contractService = contractService;
             this.blockService = blockService;
             this.transactionService = transactionService;
         }
 
-        public async Task<DeployedContractTransactionModel> DeployContractAsync(CreateContractModel createContractModel, int creatorId)
+        public async Task<DeployedContractTransactionModel> DeployContractAsync(CreatedContractModel createdContractModel, User creator)
         {
-            var createTransactionModel = mapper.Map<CreateTransactionModel>(createContractModel);
-            var transactionHash = await CreateTxnHashAsync(createContractModel.Name);
-            createTransactionModel.TxnHash = transactionHash;
+            var transactionHash = await CreateTxnHashAsync(createdContractModel.CreatorWallet);
 
-            var contract = await contractService.CreateAsync(createContractModel, creatorId);
-            createTransactionModel.InitiatorId = contract.CreatorWalletId;
-            createTransactionModel.TargetId = contract.Id;
+            var createTransactionModel = new CreateTransactionModel
+            {
+                TxnHash = transactionHash,
+                InitiatorId = createdContractModel.CreatorWalletId,
+                TargetId = createdContractModel.CreatorWalletId,
+                StateId = 1,
+            };
 
+            var createBlockModel = new CreateBlockModel
+            {
+                BaseFeePerGas = 0.000000025m,
+                GasUsed = 275345,
+                TxnHash = transactionHash
+            };
 
-            var createBlockModel = mapper.Map<CreateBlockModel>(createTransactionModel);
-            createBlockModel.BaseFeePerGas = 0.000000025m;
-            createBlockModel.GasUsed = 275345;
-            createBlockModel.TxnHash = transactionHash;
-
-            var block = await blockService.CreateAsync(createBlockModel, creatorId);
+            var block = await blockService.CreateAsync(createBlockModel, creator.Id);
 
             createTransactionModel.BlockId = block.Id;
 
-            var transaction = await transactionService.CreateAsync(createTransactionModel, creatorId);
-            //await this.transactionService.UpdateStateAsync(transaction);
-
+            var transaction = await transactionService.CreateAsync(createTransactionModel, creator.Id);
 
             var deployedContractModel = new DeployedContractTransactionModel
             {
-                ContractId = contract.Id,
+                ContractId = createdContractModel.Id,
                 TxnHash = transaction.TxnHash,
-                TxnState = createTransactionModel.State,
-                ContractName = contract.Name,
-                ContractAddress = contract.Address,
-                CreatorAddress = createContractModel.CreatorAddress,
-                OwnerAddress = createContractModel.CreatorAddress,
+                ContractName = createdContractModel.Name,
+                ContractAddress = createdContractModel.Address,
+                CreatorAddress = createdContractModel.CreatorWallet,
+                OwnerAddress = createdContractModel.OwnerWallet,
             };
 
+            await this.transactionService.UpdateStateAsync(transaction, createTransactionModel.StateId + 1, creator.Id);
 
+            deployedContractModel.TxnState = transaction.State.State;
 
             return await Task.Run(() => deployedContractModel);
         }
@@ -74,8 +71,6 @@
 
             return await Task.Run(txnHash.ToString);
         }
-
-
 
         public async Task<TransferedUnitTransactionModel> TransferAsync(TransferUnitTransactionModel initiateTransactionModel, int creatorId)
         {
