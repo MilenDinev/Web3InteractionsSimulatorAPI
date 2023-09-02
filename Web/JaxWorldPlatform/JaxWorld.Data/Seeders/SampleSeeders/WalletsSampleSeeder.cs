bf9cbf9b6f1b6c3e0e42e724ee53fc7193246dd9
MilenDinev <dinev.milen@gmail.com>
2023-09-02 19:08:35 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Seeders/SampleSeeders/WalletsSampleSeeder.cs
@@ -0,0 +1,30 @@
+﻿namespace JaxWorld.Data.Seeders.SampleSeeders
+{
+    using Entities.Wallets;
+    using Microsoft.EntityFrameworkCore;
+
+    internal class WalletsSampleSeeder
+    {
+        internal static async Task Seed(JaxWorldDbContext context)
+        {
+            var avaxNetwork = await context.Networks.FindAsync(1);
+            var AdminWallet = new Wallet
+            {
+                ProviderId = 1,
+                Address = "0x72d0672Dc344F9389aF2Df418256A82F8BEFB",
+                NormalizedTag = "0x72d0672Dc344F9389aF2Df418256A82F8BEFB".ToUpper(),
+                Balance = 0.00m,
+                OwnerId = 1,
+                CreatorId = 1,
+                CreationDate = DateTime.Now,
+                LastModifierId = 1,
+                LastModificationDate = DateTime.Now
+            };
+
+            AdminWallet.Networks.Add(avaxNetwork);
+
+            await context.Wallets.AddAsync(AdminWallet);
+            await context.SaveChangesAsync();
+        }
+    }
+}
