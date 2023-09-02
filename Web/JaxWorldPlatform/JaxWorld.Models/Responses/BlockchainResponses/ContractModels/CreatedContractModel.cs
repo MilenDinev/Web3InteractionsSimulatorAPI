5899513f8e7b3e722043f7e6a62052c4359c4433
MilenDinev <dinev.milen@gmail.com>
2023-09-02 19:12:09 +0300

    Adding Address, Creator, CreatorWalletId, CreatorWallet and OwnerWallet properties

--- Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/ContractModels/CreatedContractModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/ContractModels/CreatedContractModel.cs
@@ -3,6 +3,11 @@
     public class CreatedContractModel
     {
         public int Id { get; set; }
+        public string Address { get; set; }
         public string Name { get; set; }
+        public string Creator { get; set; }
+        public int CreatorWalletId { get; set; }
+        public string CreatorWallet { get; set; }
+        public string OwnerWallet { get; set; }
     }
 }
