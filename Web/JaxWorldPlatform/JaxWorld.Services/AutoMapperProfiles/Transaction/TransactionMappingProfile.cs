9d99b53cfa2a3bc15d358d29f4c557c862ae2856
MilenDinev <dinev.milen@gmail.com>
2023-09-02 19:18:03 +0300

    Updating mapping forCreateTransactionModel and Transaction and refactoring

--- Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Transaction/TransactionMappingProfile.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Transaction/TransactionMappingProfile.cs
@@ -2,8 +2,6 @@
 {
     using AutoMapper;
     using Data.Entities.Transactions;
-    using JaxWorld.Models.Requests.BlockchainRequests.TransactionModels;
-    using Models.Requests.BlockchainRequests;
     using Models.Requests.BlockchainRequests.TransactionModels;
     using Models.Responses.BlockchainResponses.TransactionModels;
 
@@ -12,7 +10,7 @@
         public TransactionMappingProfile()
         {
             this.CreateMap<CreateTransactionModel, Transaction>()
-                .ForMember(e => e.NormalizedTag, m => m.MapFrom(m => m.State.ToUpper()));
+                .ForMember(e => e.NormalizedTag, m => m.MapFrom(m => m.TxnHash.ToUpper()));
             this.CreateMap<Transaction, DeployedContractTransactionModel>();
             this.CreateMap<Transaction, TransactionListingModel>();
             this.CreateMap<Transaction, TransactionListingModel>();
