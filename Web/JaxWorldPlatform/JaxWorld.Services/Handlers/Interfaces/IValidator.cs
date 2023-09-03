4578ddc33f18b4899fdcf112f64e296045b92e8e
MilenDinev <dinev.milen@gmail.com>
2023-09-04 00:06:58 +0300

    Updating ValidateEntityAsync arguments, and declaring ValidateWalletOwnershipAsync

--- Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/IValidator.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/IValidator.cs
@@ -1,10 +1,13 @@
 ﻿namespace JaxWorld.Services.Handlers.Interfaces
 {
+    using Data.Entities;
+    using Data.Entities.Wallets;
     using Data.Interfaces.IEntities;
 
     public interface IValidator
     {
-        Task ValidateEntityAsync<T>(T entity, string flag) where T : class, IEntity;
+        Task ValidateEntityAsync<T>(T entity) where T : class, IEntity;
         Task ValidateUniqueEntityAsync<T>(T entity) where T : class, IEntity;
+        Task ValidateWalletOwnershipAsync(User Owner, Wallet wallet);
     }
 }
