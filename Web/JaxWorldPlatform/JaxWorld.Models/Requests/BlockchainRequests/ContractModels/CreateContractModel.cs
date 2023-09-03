3fdb3c69bbd1e1e2fad2ca427c2f024d3e1953ce
MilenDinev <dinev.milen@gmail.com>
2023-09-04 00:03:37 +0300

    Creating CreatorAddress with validation attributes

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/CreateContractModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/ContractModels/CreateContractModel.cs
@@ -11,6 +11,11 @@
             MinimumLength = AttributesParams.ContractNameMinLength)]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [MinLength(AttributesParams.WalletAddressMinLength,
+            ErrorMessage = ValidationMessages.MinLength)]
+        public string CreatorAddress { get; set; }
+
         [RegularExpression("(^(?i)1$|^2$|^3$)",
             ErrorMessage = ValidationMessages.Network)]
         public int NetworkId { get; set; }
