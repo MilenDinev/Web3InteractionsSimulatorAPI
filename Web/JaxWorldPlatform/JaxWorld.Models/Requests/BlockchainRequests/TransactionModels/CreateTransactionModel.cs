bd3185daa16e1f400534f4c9a8da4ac70cbfe870
MilenDinev <dinev.milen@gmail.com>
2023-09-04 00:04:44 +0300

    Adding CreatorId , NetworkId, InitiatorWalletId properties and commented TxnActionId property

--- Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/TransactionModels/CreateTransactionModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/TransactionModels/CreateTransactionModel.cs
@@ -2,12 +2,12 @@
 {
     public class CreateTransactionModel
     {
-        public string TxnHash { get; set; }
-        public int StateId { get; set; }
-        public int TargetId { get; set; }
-        public int InitiatorId { get; set; }
+        public int CreatorId { get; set; }
         public int NetworkId { get; set; }
+        public string TxnHash { get; set; }
+        public int InitiatorWalletId { get; set; }
+        public int StateId { get; set; }
         public int BlockId { get; set; }
-        public int TxnActionId { get; set; }
+        //public int TxnActionId { get; set; }
     }
 }
