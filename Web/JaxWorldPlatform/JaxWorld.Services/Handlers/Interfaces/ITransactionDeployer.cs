ab6fcebe1a5344e97766e8b47f5f64c873543a61
MilenDinev <dinev.milen@gmail.com>
2023-09-04 21:59:14 +0300

    Declaring DeployProfileTxnAsync method

--- Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/ITransactionDeployer.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/ITransactionDeployer.cs
@@ -1,12 +1,14 @@
 ﻿namespace JaxWorld.Services.Handlers.Interfaces
 {
+    using Models.Responses.BlockchainResponses.ProfileModels;
+    using Models.Responses.BlockchainResponses.ContractModels;
     using Models.Requests.BlockchainRequests.TransactionModels;
     using Models.Responses.BlockchainResponses.TransactionModels;
 
     public interface ITransactionDeployer
     {
         Task<DeployedContractTxnModel> DeployContractTxnAsync(CreatedContractModel createdContractModel, int creatorId);
-        Task<DeployedContractTransactionModel> DeployTransactionAsync(CreateTransactionModel createTransactionModel, int targetContractId);
+        Task<DeployedProfileTxnModel> DeployProfileTxnAsync(CreatedProfileModel createdProfileModel, int creatorId);
         Task<TransferedUnitTransactionModel> TransferAsync(TransferUnitTransactionModel initiateTransactionModel, int creatorId);
         Task<ClaimedUnitTransactionModel> ClaimAsync(ClaimUnitTransactionModel initiateTransactionModel, int creatorId);
     }
