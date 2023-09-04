7d1606461c637495cab7b00c71eaae8b6258e917
MilenDinev <dinev.milen@gmail.com>
2023-09-04 21:56:09 +0300

    Renaming

--- Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/TransactionModels/DeployedContractTxnModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/TransactionModels/DeployedContractTxnModel.cs
@@ -1,11 +1,11 @@
 ﻿namespace JaxWorld.Models.Responses.BlockchainResponses.TransactionModels
 {
-    public class DeployedContractTransactionModel
+    public class DeployedContractTxnModel
     {
-        public int ContractId { get; set; }
         public int BlockId { get; set; }
         public string TxnHash { get; set; }
         public string TxnState { get; set; }
+        public int ContractId { get; set; }
         public string ContractName { get; set; }
         public string ContractAddress { get; set; }
         public string CreatorAddress { get; set; }
