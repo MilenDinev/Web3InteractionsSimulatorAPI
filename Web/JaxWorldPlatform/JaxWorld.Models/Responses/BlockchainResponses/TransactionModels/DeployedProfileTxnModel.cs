cf77381fa0c1c716ae7bb04d7e05106eb92472c1
MilenDinev <dinev.milen@gmail.com>
2023-09-04 21:56:01 +0300

    Initial creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/TransactionModels/DeployedProfileTxnModel.cs
@@ -0,0 +1,13 @@
+﻿namespace JaxWorld.Models.Responses.BlockchainResponses.TransactionModels
+{
+    public class DeployedProfileTxnModel
+    {
+        public int ProfileId { get; set; }
+        public string ProfileName { get; set; }
+        public string Standard { get; set; }
+        public string Symbol { get; set; }
+        public string Description { get; set; }
+        public int TotalSupply { get; set; }
+        public string ContractAddress { get; set; }
+    }
+}
