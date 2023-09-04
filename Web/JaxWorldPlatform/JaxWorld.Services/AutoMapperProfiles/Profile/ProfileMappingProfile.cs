08fcb55e1df2107f18a6530e1ce0fad0132fa14b
MilenDinev <dinev.milen@gmail.com>
2023-09-04 21:57:34 +0300

    Updating Profile x CreateProfileModel  and CreatedProfile x Profile mappings

--- Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Profile/ProfileMappingProfile.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/AutoMapperProfiles/Profile/ProfileMappingProfile.cs
@@ -9,8 +9,12 @@
         public ProfileMappingProfile()
         {
             this.CreateMap<CreateProfileModel, Profile>()
-                .ForMember(e => e.NormalizedTag, m => m.MapFrom(m => m.Name.ToUpper()));
-            this.CreateMap<Profile, CreatedProfileModel>();
+                .ForMember(e => e.NormalizedTag, m => m.MapFrom(m => m.Name.ToUpper()))
+                .ForMember(e => e.ContractId, m => m.Ignore())
+                .ForMember(e => e.StandardId, m => m.Ignore());
+            this.CreateMap<Profile, CreatedProfileModel>()
+                .ForMember(m => m.CreatorWalletId, e => e.MapFrom(e => e.Contract.CreatorWalletId))
+                .ForMember(m => m.NetworkId, e => e.MapFrom(e => e.Contract.NetworkId));
             this.CreateMap<Profile, EditedProfileModel>();
             this.CreateMap<Profile, DeletedProfileModel>();
             this.CreateMap<Profile, ProfileListingModel>();
