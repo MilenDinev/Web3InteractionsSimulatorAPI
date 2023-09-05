1e23cc554aa59383a144dcf994b5c2790d8e2829
MilenDinev <dinev.milen@gmail.com>
2023-09-06 00:42:26 +0300

    Declarating MintedErc721aUnitTxnAsync method

--- Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/ITransactionDeployer.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/ITransactionDeployer.cs
@@ -4,11 +4,13 @@
     using Models.Responses.BlockchainResponses.ContractModels;
     using Models.Requests.BlockchainRequests.TransactionModels;
     using Models.Responses.BlockchainResponses.TransactionModels;
+    using Models.Responses.BlockchainResponses.ProfileUnitModels;
 
     public interface ITransactionDeployer
     {
         Task<DeployedContractTxnModel> DeployContractTxnAsync(CreatedContractModel createdContractModel, int creatorId);
         Task<DeployedProfileTxnModel> DeployProfileTxnAsync(CreatedProfileModel createdProfileModel, int creatorId);
+        Task<MintedErc721aUnitTxnModel> MintedErc721aUnitTxnAsync(CreatedErc721aUnitModel createdErc721aModel, int creatorId);
         Task<TransferedUnitTransactionModel> TransferAsync(TransferUnitTransactionModel initiateTransactionModel, int creatorId);
         Task<ClaimedUnitTransactionModel> ClaimAsync(ClaimUnitTransactionModel initiateTransactionModel, int creatorId);
     }
