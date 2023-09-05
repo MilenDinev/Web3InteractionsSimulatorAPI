1db2dcc4490cb3f768856326fae1ae8767062621
MilenDinev <dinev.milen@gmail.com>
2023-09-05 23:43:31 +0300

    Making Holder and ImageUrl nullable

--- Web/JaxWorldPlatform/JaxWorld.Data/Entities/Units/Erc721aUnit.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Units/Erc721aUnit.cs
@@ -15,7 +15,7 @@
             Transactions = new HashSet<Transaction>();
         }
         public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
         public string? ExternalUrl { get; set; }
         public bool Minted { get; set; }
         public string? MintedTxnHash { get; set; }
@@ -25,7 +25,7 @@
         public virtual ICollection<Attribute> Attributes { get; set; }
         public virtual ICollection<Utility> Utilities { get; set; }
         public int? HolderId { get; set; }
-        public virtual Wallet Holder { get; set; }
+        public virtual Wallet? Holder { get; set; }
         public virtual ICollection<Transaction> Transactions { get; set; }
     }
 }
