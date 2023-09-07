ce3bcb1b364d0a539ac4ab060d788e46ff2c5801
MilenDinev <dinev.milen@gmail.com>
2023-09-08 00:02:13 +0300

    Updating ListingModel

--- Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/PropertiesModels/Attribute/AttributeListingModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/PropertiesModels/Attribute/AttributeListingModel.cs
@@ -4,5 +4,6 @@
     {
         public int Id { get; set; }
         public string TraitType { get; set; }
+        public string Value { get; set; }
     }
 }
--- Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/UnitModels/Erc721aUnitListingModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/UnitModels/Erc721aUnitListingModel.cs
@@ -2,5 +2,11 @@
 {
     public class Erc721aUnitListingModel
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string HolderAddress { get; set; }
+        public string ContractAddress { get; set; }
+        public string? ExternalUrl { get; set; }
     }
 }
