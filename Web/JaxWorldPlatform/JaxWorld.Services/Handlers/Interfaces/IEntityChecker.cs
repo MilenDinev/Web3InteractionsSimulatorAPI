59dc3a0d9648f741709a77eaaa19a3870c849011
MilenDinev <dinev.milen@gmail.com>
2023-09-09 22:25:13 +0300

    Deleting as is not in use

--- Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/IEntityChecker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JaxWorld.Services.Handlers.Interfaces
-{
-    using Data.Interfaces.IEntities;
-    public interface IEntityChecker
-    {
-        Task<bool> NullableCheck<T>(T entity) where T : class, IEntity;
-        Task<bool> DeletedCheck<T>(T entity) where T : class, IEntity;
-    }
-}
