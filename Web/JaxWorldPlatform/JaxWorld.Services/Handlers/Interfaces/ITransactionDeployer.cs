358b57cf2ff7fcbee0db4630b77ea5827f6c7500
MilenDinev <dinev.milen@gmail.com>
2023-09-15 21:22:15 +0300

    Removing DeployCOntractTxnAsync

--- Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/ITransactionDeployer.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/ITransactionDeployer.cs
@@ -1,12 +1,10 @@
 ﻿namespace JaxWorld.Services.Handlers.Interfaces
 {
-    using Models.Responses.BlockchainResponses.ContractModels;
     using Models.Responses.BlockchainResponses.ProfileModels;
     using Models.Responses.BlockchainResponses.ProfileUnitModels;
 
     public interface ITransactionDeployer
     {
-        Task DeployContractTxnAsync(CreatedContractModel createdContractModel, int creatorId, int initiatorWalletId);
         Task DeployProfileTxnAsync(CreatedProfileModel createdProfileModel, int creatorId);
         Task MintErc721aUnitTxnAsync(CreatedErc721aUnitModel createdErc721aModel, int creatorId);
     }
