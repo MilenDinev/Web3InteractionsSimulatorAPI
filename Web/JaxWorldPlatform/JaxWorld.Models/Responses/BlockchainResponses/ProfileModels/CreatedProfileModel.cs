9583ae363060cf5e413c6fd43080038f49c0b57d
MilenDinev <dinev.milen@gmail.com>
2023-09-15 21:20:52 +0300

    Implementing ICreated

--- Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/ProfileModels/CreatedProfileModel.cs
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/ProfileModels/CreatedProfileModel.cs
@@ -1,8 +1,13 @@
 ﻿namespace JaxWorld.Models.Responses.BlockchainResponses.ProfileModels
 {
-    public class CreatedProfileModel
+    using Interfaces;
+
+    public class CreatedProfileModel : ICreated
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int NetworkId { get; set; }
+        public int CreatorId { get; set; }
+        public int CreatorWalletId { get; set; }
     }
 }
