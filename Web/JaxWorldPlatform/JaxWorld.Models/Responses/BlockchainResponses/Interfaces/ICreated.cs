8d31da4f8fec81afe299255afd815f34faa40db7
MilenDinev <dinev.milen@gmail.com>
2023-09-15 21:20:36 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Responses/BlockchainResponses/Interfaces/ICreated.cs
@@ -0,0 +1,10 @@
+﻿namespace JaxWorld.Models.Responses.BlockchainResponses.Interfaces
+{
+    public interface ICreated
+    {
+        public int Id { get; set; }
+        public int NetworkId { get; set; }
+        public int CreatorId { get; set; }
+        public int CreatorWalletId { get; set; }
+    }
+}
