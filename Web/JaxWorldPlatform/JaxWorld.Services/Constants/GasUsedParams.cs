39222d543f4a532e89da8d24a1311408679b86f4
MilenDinev <dinev.milen@gmail.com>
2023-09-15 21:21:33 +0300

    Adding ProfileDeployGas and Erc721aMintGas

--- Web/JaxWorldPlatform/JaxWorld.Services/Constants/GasUsedParams.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Constants/GasUsedParams.cs
@@ -3,5 +3,7 @@
     internal static class GasUsedParams
     {
         internal const long ContractDeployGas = 525345;
+        internal const long ProfileDeployGas = 423421;
+        internal const long Erc721aMintGas = 325345;
     }
 }
