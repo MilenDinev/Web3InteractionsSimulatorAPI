de697775a93e59d71c80d63e0737116ef74c3ce7
MilenDinev <dinev.milen@gmail.com>
2023-09-16 13:06:23 +0300

    Initial Create

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Constants/TransactionStates.cs
@@ -0,0 +1,10 @@
+﻿namespace JaxWorld.Services.Constants
+{
+    internal static class TransactionStates
+    {
+        internal const string Rejected = "Rejected";
+        internal const string Pending = "Pending";
+        internal const string Confirmed = "Confirmed";
+        internal const string Completed = "Completed";
+    }
+}
