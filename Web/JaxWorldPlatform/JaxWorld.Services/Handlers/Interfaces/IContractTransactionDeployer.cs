f794d1cb4be86a022d7915fe0cba400e362952d3
MilenDinev <dinev.milen@gmail.com>
2023-09-16 13:06:32 +0300

    Initial Create

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/IContractTransactionDeployer.cs
@@ -0,0 +1,11 @@
+﻿namespace JaxWorld.Services.Handlers.Interfaces
+{
+    using Data.Entities;
+    using Models.Requests.BlockchainRequests.ContractModels;
+    using Models.Responses.BlockchainResponses.ContractModels;
+
+    public interface IContractTransactionDeployer
+    {
+        Task<CreatedContractModel> DeployContractTxnAsync(CreateContractModel createContractModel, User user);
+    }
+}
