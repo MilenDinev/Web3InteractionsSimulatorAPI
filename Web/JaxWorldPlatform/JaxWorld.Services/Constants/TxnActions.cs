06f2e838fc6cc8c4c5a919de951ff01d28263170
MilenDinev <dinev.milen@gmail.com>
2023-09-19 23:56:30 +0300

    Initial Create

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Constants/TxnActions.cs
@@ -0,0 +1,10 @@
+﻿namespace JaxWorld.Services.Constants
+{
+    internal static class TxnActions
+    {
+        internal const string Mint = "Mint";
+        internal const string Deploy = "Deploy";
+        internal const string Claim = "Claim";
+        internal const string Transfer = "Transfer";
+    }
+}
