76fa9b2ac48f70b391cca7739290d84e61ded6d4
MilenDinev <dinev.milen@gmail.com>
2023-09-19 23:52:05 +0300

    recatoring

--- Web/JaxWorldPlatform/JaxWorld.Services/Constants/ErrorMessages.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Constants/ErrorMessages.cs
@@ -5,7 +5,7 @@
         internal const string InvalidCredentials = "Invalid credentials, please try again!";
         internal const string AddresableEntityAlreadyExists = @"{0} with address:'{1}' already exists!";
         internal const string NamedEntityAlreadyExists = @"{0} with name:'{1}' already exists!";
-        internal const string NotAvailableWalletBalnce = @"{0} : does not have required balance to perform this action!";
+        internal const string NotAvailableWalletBalance = @"{0} : does not have required balance to perform this action!";
         internal const string EntityDoesNotExist = @"{0} does not exist!";
         internal const string EntityAlreadyExists = @"{0} already exists in our system!";
     }
