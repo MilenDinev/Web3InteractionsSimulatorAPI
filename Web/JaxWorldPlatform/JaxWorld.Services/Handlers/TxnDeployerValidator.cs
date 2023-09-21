75647dec9e689ea58508ad8f75113aeb792a0b84
MilenDinev <dinev.milen@gmail.com>
2023-09-21 23:56:19 +0300

    Initial Creation and implementation of ITxnDeployerValidator

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Handlers/TxnDeployerValidator.cs
@@ -0,0 +1,59 @@
+﻿namespace JaxWorld.Services.Handlers
+{
+    using JaxWorld.Data.Entities.Contracts;
+    using JaxWorld.Data.Entities.Wallets;
+    using JaxWorld.Services.Constants;
+    using JaxWorld.Services.Handlers.Exceptions;
+    using JaxWorld.Services.Handlers.Interfaces;
+
+    public class TxnDeployerValidator : ITxnDeployerValidator
+    {
+        private readonly IFinder finder;
+
+        public TxnDeployerValidator(IFinder finder)
+        {
+            this.finder = finder;
+        }
+
+        public async Task<bool> ValidateWalletAsync(Wallet wallet, string contractAddress, decimal cost)
+        {
+            var isValidWallet = await ValidateWalletApprovalAsync(wallet.Address, contractAddress)
+                && await ValidateWalletBalanceAsync(wallet.Balance, cost);
+
+            return isValidWallet;
+        }
+
+        public async Task<string> GetValidContractAddressAsync(int contractId)
+        {
+            var contract = await this.finder.FindByIdOrDefaultAsync<Contract>(contractId)
+            ?? throw new ResourceNotFoundException(string.Format(ErrorMessages.EntityDoesNotExist,
+            typeof(Contract).Name));
+            return contract.Address;
+        }
+
+        public async Task<bool> ValidateContractOwner(Wallet wallet, int contractId)
+        {
+            return await Task.Run(() => wallet.CreatedContracts.Any(c => c.Id == contractId));
+        }
+
+        private async Task<bool> ValidateWalletApprovalAsync(string walletAddress, string contractAddress)
+        {
+            var contract = await this.finder.FindByStringOrDefaultAsync<Contract>(contractAddress);
+            var isValidWallet = false; 
+
+            if (contract != null)
+            {
+                isValidWallet = contract.ApprovedBy.Any(x => x.Address == walletAddress);
+            }
+
+            return isValidWallet;
+        }
+
+        private static async Task<bool> ValidateWalletBalanceAsync(decimal walletBalance, decimal cost)
+        {
+            var isValidWallet = await Task.Run(() => walletBalance - cost >= 0.00000000M);
+
+            return isValidWallet;
+        }
+    }
+}
