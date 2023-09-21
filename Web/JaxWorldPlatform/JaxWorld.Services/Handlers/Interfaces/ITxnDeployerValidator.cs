e0e3ac521c5cc1dcfe642385a120fcded295ac74
MilenDinev <dinev.milen@gmail.com>
2023-09-21 23:55:55 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Services/Handlers/Interfaces/ITxnDeployerValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace JaxWorld.Services.Handlers.Interfaces
+{
+    using Data.Entities.Wallets;
+
+    public interface ITxnDeployerValidator
+    {
+        Task<bool> ValidateWalletAsync(Wallet wallet, string contractAddress, decimal cost);
+        Task<string> GetValidContractAddressAsync(int contractId);
+        Task<bool> ValidateContractOwner(Wallet wallet, int contractId);
+    }
+}
