a672cc2a3db6ffeccf710bdda6183a4369b054e8
MilenDinev <dinev.milen@gmail.com>
2023-09-22 16:37:42 +0300

    Adding DeployMultiplierGas and MintMultiplierGas constants

--- Web/JaxWorldPlatform/JaxWorld.Services/Constants/GasUsedParams.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Constants/GasUsedParams.cs
@@ -5,5 +5,7 @@
         internal const long ContractDeployGas = 525345;
         internal const long ProfileDeployGas = 423421;
         internal const long Erc721aMintGas = 325345;
+        internal const decimal DeployMultiplierGas = 0.00000015M;
+        internal const decimal MintMultiplierGas = 0.00000002M;
     }
 }
