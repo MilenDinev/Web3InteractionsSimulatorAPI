5470f52cc18455ded24dbe3ae1dd6744aa856ca9
MilenDinev <dinev.milen@gmail.com>
2023-09-22 16:37:28 +0300

    Adding OperationExecutionRejected and ContractNotApproved eeror messages

--- Web/JaxWorldPlatform/JaxWorld.Services/Constants/ErrorMessages.cs
+++ Web/JaxWorldPlatform/JaxWorld.Services/Constants/ErrorMessages.cs
@@ -3,9 +3,11 @@
     public static class ErrorMessages
     {
         internal const string InvalidCredentials = "Invalid credentials, please try again!";
+        internal const string OperationExecutionRejected = "Operation has been rejected!";
         internal const string AddresableEntityAlreadyExists = @"{0} with address:'{1}' already exists!";
         internal const string NamedEntityAlreadyExists = @"{0} with name:'{1}' already exists!";
         internal const string NotAvailableWalletBalance = @"{0} : does not have required balance to perform this action!";
+        internal const string ContractNotApproved = @"{0} has not been approved by {1}!";
         internal const string EntityDoesNotExist = @"{0} does not exist!";
         internal const string EntityAlreadyExists = @"{0} already exists in our system!";
     }
