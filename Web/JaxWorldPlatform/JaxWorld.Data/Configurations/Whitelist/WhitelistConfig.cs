61a75bdd5c26c4ef180dc6cf75e3c6e1cee41cd2
MilenDinev <dinev.milen@gmail.com>
2023-09-24 02:46:53 +0300

    Initial Creation

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Whitelist/WhitelistConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace JaxWorld.Data.Configurations.Whitelist
+{
+    using Entities.Whitelists;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    internal class WhitelistConfig : IEntityTypeConfiguration<Whitelist>
+    {
+        public void Configure(EntityTypeBuilder<Whitelist> builder)
+        {
+
+            builder.HasOne(w => w.Creator)
+                .WithMany()
+                .HasForeignKey(w => w.CreatorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(w => w.LastModifier)
+                .WithMany()
+                .HasForeignKey(w => w.LastModifierId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Configurations/Whitelist/WhitelistStatusConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace JaxWorld.Data.Configurations.WhitelistStatus
+{
+    using Entities.Whitelists;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    internal class WhitelistStatusConfig : IEntityTypeConfiguration<WhitelistStatus>
+    {
+        public void Configure(EntityTypeBuilder<WhitelistStatus> builder)
+        {
+
+            builder.HasOne(w => w.Creator)
+                .WithMany()
+                .HasForeignKey(w => w.CreatorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(w => w.LastModifier)
+                .WithMany()
+                .HasForeignKey(w => w.LastModifierId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
