8ba56757e2efab9960da4490ffd6438f99da9675
MilenDinev <dinev.milen@gmail.com>
2023-09-24 00:22:03 +0300

    Adding WhitelistStatusesSeeder.Seed

--- Web/JaxWorldPlatform/JaxWorld.Data/Seeders/DatabaseSeeder.cs
+++ Web/JaxWorldPlatform/JaxWorld.Data/Seeders/DatabaseSeeder.cs
@@ -1,15 +1,15 @@
 ﻿namespace JaxWorld.Data.Seeders
 {
-    using Entities;
-    using EntitySeeders;
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.Extensions.DependencyInjection;
-    using SampleSeeders;
     using System;
-    using System.Diagnostics.CodeAnalysis;
     using System.Linq;
     using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Identity;
+    using System.Diagnostics.CodeAnalysis;
+    using Microsoft.Extensions.DependencyInjection;
+    using Entities;
+    using EntitySeeders;
+    using SampleSeeders;
 
     [ExcludeFromCodeCoverage]
     public static class DatabaseSeeder
@@ -32,6 +32,7 @@
                     await ProviderSeeder.Seed(context);
                     await NetworksSampleSeeder.Seed(context);
                     await WalletsSampleSeeder.Seed(context);
+                    await WhitelistStatusesSeeder.Seed(context);
                     await TransactionStateSeeder.Seed(context);
                     await TxnActionSeeder.Seed(context);
                     await ContractSeeder.Seed(context);
