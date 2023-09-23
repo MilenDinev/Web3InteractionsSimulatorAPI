1bb300bba308eec02d93c3045deb9ab958af52f1
MilenDinev <dinev.milen@gmail.com>
2023-09-24 00:19:10 +0300

    Initial Create

---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Whitelists/Whitelist.cs
@@ -0,0 +1,20 @@
+﻿namespace JaxWorld.Data.Entities.Whitelists
+{
+    using Base;
+    using Wallets;
+
+    public class Whitelist : Entity
+    {
+        public Whitelist()
+        {
+            this.WhitelistedWallets = new HashSet<Wallet>();       
+        }
+
+        public int Id { get; set; }
+        public int ProfileId { get; set; }
+        public virtual Profile Profile { get; set; }
+        public int StatusId { get; set; }
+        public virtual WhitelistStatus Status { get; set; }
+        public virtual ICollection<Wallet> WhitelistedWallets { get; set; }
+    }
+}
---
+++ Web/JaxWorldPlatform/JaxWorld.Data/Entities/Whitelists/WhitelistStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace JaxWorld.Data.Entities.Whitelists
+{
+    using Base;
+
+    public class WhitelistStatus : Entity
+    {
+        public WhitelistStatus()
+        {
+            this.Whitelists = new HashSet<Whitelist>();    
+        }
+
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public virtual ICollection<Whitelist> Whitelists { get; set; }
+    }
+}
