8cf6c27ad03dba07b842cb016c3fd964cc2714ef
MilenDinev <dinev.milen@gmail.com>
2023-09-24 23:26:50 +0300

    Initial Create

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/UnitModels/BuyUnitModel.cs
@@ -0,0 +1,8 @@
+﻿namespace JaxWorld.Models.Requests.BlockchainRequests.UnitModels
+{
+    public class BuyUnitModel
+    {
+        public int UnitId { get; set; }
+        public decimal Price { get; set; }
+    }
+}
