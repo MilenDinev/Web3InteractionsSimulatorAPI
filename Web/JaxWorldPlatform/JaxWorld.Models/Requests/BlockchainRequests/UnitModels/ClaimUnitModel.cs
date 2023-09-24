f5260871164afbbb943ee64bb98b70c44ff21922
MilenDinev <dinev.milen@gmail.com>
2023-09-24 23:26:54 +0300

    Initial Create

---
+++ Web/JaxWorldPlatform/JaxWorld.Models/Requests/BlockchainRequests/UnitModels/ClaimUnitModel.cs
@@ -0,0 +1,8 @@
+﻿namespace JaxWorld.Models.Requests.BlockchainRequests.UnitModels
+{
+    public class ClaimUnitModel
+    {
+        public int UnitId { get; set; }
+        public int ProfileId { get; set; }
+    }
+}
